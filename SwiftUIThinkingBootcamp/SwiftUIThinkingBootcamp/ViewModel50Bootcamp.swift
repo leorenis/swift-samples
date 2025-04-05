6726cc60484786be6124f86170be4c6674a8eb3d
leorenis <leorenis@gmail.com>
2025-04-05 12:28:07 -0300

    learn: swift - refactoring ViewModel to separate concerns. Remove the logic from the view

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ViewModel50Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ViewModel50Bootcamp.swift
@@ -15,34 +15,15 @@ struct FruitModel: Identifiable {
     let count: Int
 }
 
-
-struct ViewModel50Bootcamp: View {
+/// A class to provide data access for our fruits.
+///
+class FruitViewModel: ObservableObject {
+    // @Published: for now, this is the same thing as the @State, except it's whitin a class.
+    @Published var fruits: [FruitModel] = []
+    @Published var isLoading: Bool = false
     
-    // MARK: PROPERTIES
-    @State var fruits: [FruitModel] = []
-    
-    /// View Body: block is responsable for acts as entry point in this view
-    var body: some View {
-        
-        NavigationStack {
-            List {
-                ForEach(fruits) { fruit in
-                    HStack {
-                        Text(fruit.name)
-                        Spacer()
-                        Text("\(fruit.count)")
-                            .font(.headline)
-                            .foregroundStyle(.primary.opacity(0.5))
-                    }
-                }
-                .onDelete(perform: deleteFruit)
-            }
-            .listStyle(InsetGroupedListStyle())
-            .navigationTitle(Text("Fruits"))
-            .onAppear {
-                fillFruits()
-            }
-        }
+    init() {
+        fillFruits()
     }
     
     // MARK: FUNCTIONS
@@ -63,7 +44,12 @@ struct ViewModel50Bootcamp: View {
         let fruitFive = FruitModel(name: "Watermelon", count: 1)
         let fruitSix = FruitModel(name: "Banana", count: 9)
         
-        fruits.append(contentsOf: [fruitOne, fruitTwo, fruitThree, fruitFour, fruitFive, fruitSix])
+        // This is just to simulate a slowly database load...
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.fruits.append(contentsOf: [fruitOne, fruitTwo, fruitThree, fruitFour, fruitFive, fruitSix])
+            self.isLoading = false
+        }
     }
     
     /// performs remove method in array to delete  some fruit at IndexSet from fruits array.
@@ -76,6 +62,102 @@ struct ViewModel50Bootcamp: View {
     ///
     func deleteFruit(indexOffSet: IndexSet) {
         fruits.remove(atOffsets: indexOffSet)
+    }
+}
+
+struct ViewModel50Bootcamp: View {
+    
+    // MARK: PROPERTIES
+    
+    /**
+     * Generic struct 'ObservedObject' requires that 'FruitViewModel' conform to 'ObservableObject'. E.g. FruitViewModel: ObservableObject
+     *
+     * Important Tip: When object fruitViewModel changes, this observed will change as well. But, if this current view change? In this case, you should use @StateObject instead @ObservedObject.
+     *
+     * @StateObject -> USE THIS ON CREATION or INIT.
+     * @ObservedObject -> USE THIS FOR SUBVIEWS. E.g. FruitDetailView
+     */
+    // @ObservedObject var fruitViewModel: FruitViewModel = FruitViewModel()
+    @StateObject var fruitViewModel: FruitViewModel = FruitViewModel()
+    // @State var fruits: [FruitModel] = []
+    
+    /// View Body: block is responsable for acts as entry point in this view.
+    var body: some View {
+        NavigationStack {
+            List {
+                if fruitViewModel.isLoading {
+                    ProgressView()
+                        .frame(maxWidth: .infinity)
+                } else {
+                    ForEach(fruitViewModel.fruits) { fruit in
+                        NavigationLink(
+                            destination: FruitDetailView(
+                                selected: fruit,
+                                fruitViewModel: fruitViewModel
+                            )) {
+                            HStack {
+                                Text("\(fruit.count)")
+                                    .font(.headline)
+                                    .foregroundStyle(.primary.opacity(0.5))
+                                Text(fruit.name)
+                            }
+                        }
+                    }
+                    .onDelete(perform: fruitViewModel.deleteFruit)
+                }
+                
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("Fruits")
+            /*.onAppear {
+                fruitViewModel.fillFruits() // Prefer fillFruits() on init from fruitViewModel
+            }*/
+        }
+    }
+}
+
+/// A  struct to provide Fuit Details View.
+/// - Parameter selected: current fruit in visualization.
+/// - Parameter fruitViewModel: @ObservedObject to provide array of Fruits in SubView.
+///
+struct FruitDetailView: View {
+    
+    // MARK: PROPERTIES
+    let selected: FruitModel
+    @ObservedObject var fruitViewModel: FruitViewModel
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ZStack {
+            // Background color
+            Color("AdaptiveColor")
+                .ignoresSafeArea(edges: .all)
+            
+          // Content layer
+            VStack {
+                Text("Fruit detail 🍉")
+                    .font(.title)
+                    .foregroundStyle(.secondary)
+                
+                Text("Fruit: \(selected.name) - \(selected.count)")
+                    .font(.headline)
+                    .foregroundStyle(.secondary)
+                    .padding(.bottom, 4)
+                
+                Text("All fruits: \(fruitViewModel.fruits.map(\.name).joined(separator: ", "))")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                
+                Button  {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("< Back to home")
+                        .font(.headline)
+                        .foregroundStyle(.primary)
+                        .padding()
+                }
+            }
+        }
     }
 }
 
