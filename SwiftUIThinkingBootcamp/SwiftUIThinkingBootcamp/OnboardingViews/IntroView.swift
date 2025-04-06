cd650f9e71cd0f1ed778e435929ed5fde212a1c1
leorenis <leorenis@gmail.com>
2025-04-06 06:43:18 -0300

    learn: swift - Onboarding: IntroView first commit.

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/OnboardingViews/IntroView.swift
@@ -0,0 +1,29 @@
+//
+//  IntroView.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo on 06/04/25.
+//
+
+import SwiftUI
+
+/// A class to provide  access for Onboarding Intro View.
+///
+struct IntroView: View {
+    
+    // MARK: PROPERTIES
+    
+    /// View Body: block  responsable for acts as entry point in this view.
+    var body: some View {
+        ZStack {
+            // Background
+            
+            // Foreground
+        }
+    }
+}
+
+// MARK: PREVIEW
+#Preview {
+    IntroView()
+}
