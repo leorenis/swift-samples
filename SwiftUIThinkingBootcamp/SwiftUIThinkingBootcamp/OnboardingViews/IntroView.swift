ea2a296f21057f4d8ee89cfa27edba16aa842a07
leorenis <leorenis@gmail.com>
2025-04-06 06:50:34 -0300

    learn: swift - Onboarding: IntroView LinearGradient.

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/OnboardingViews/IntroView.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/OnboardingViews/IntroView.swift
@@ -17,6 +17,13 @@ struct IntroView: View {
     var body: some View {
         ZStack {
             // Background
+            RadialGradient(
+                gradient: Gradient(colors: [Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
+                center: .topLeading,
+                startRadius: 5.0,
+                endRadius: UIScreen.main.bounds.height
+            )
+                .ignoresSafeArea(.all)
             
             // Foreground
         }
