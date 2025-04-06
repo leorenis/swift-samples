5015110700b9620a0b6bacd35aebdcbb5c3fc01a
leorenis <leorenis@gmail.com>
2025-04-05 21:50:53 -0300

    learn: swift - AppStorage: setup to coding UserDefaults.standards.set

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AppStorage52Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AppStorage52Bootcamp.swift
@@ -1,18 +1,37 @@
 //
 //  AppStorage52Bootcamp.swift
 //  SwiftUIThinkingBootcamp
+//  Episode: https://www.youtube.com/watch?v=zyuSUrfelw8&list=PLwvDm4VfkdphqETTBf-DdjCoAvhai1QpO&index=53
 //
 //  Created by Leo on 05/04/25.
 //
 
 import SwiftUI
 
+/// A class to provide data access for our Environment.
+/// 
 struct AppStorage52Bootcamp: View {
+    
+    // MARK: PROPERTIES
+    @State var currentUsername: String? = nil
+    
+    /// View Body: block is responsable for acts as entry point in this view.
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text(currentUsername ?? "Add username")
+            
+            if let username = currentUsername {
+                Text(username)
+            }
+            
+            Button("Save") {
+                currentUsername = "LRS"
+            }
+        }
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     AppStorage52Bootcamp()
 }
--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/SwiftUIThinkingBootcampApp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/SwiftUIThinkingBootcampApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIThinkingBootcampApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppStorage52Bootcamp()
         }
     }
 }
