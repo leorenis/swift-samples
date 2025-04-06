c5f2abf4ba858bf1463950cf2d4b2f608abaf663
leorenis <leorenis@gmail.com>
2025-04-06 09:15:07 -0300

    learn: swift - Onboarding: IntroView Onboarding sections.

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/OnboardingViews/OnboardingView.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/OnboardingViews/OnboardingView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 ///
 struct OnboardingView: View {
     // MARK: PROPERTIES
-    
+
     // Onboarding states:
     /**
      * 0 - Welcome screen
@@ -19,14 +19,21 @@ struct OnboardingView: View {
      * 2 - Add Age
      * 3 - Add gender
      */
-    @State var onboardingState: Int = 0
+    @State var onboardingState: Int = 2
+    @State var textFieldName: String = ""
+    @State var slideAge: CGFloat = 25
     
     /// View Body: block  responsable for acts as entry point in this view.
     var body: some View {
         ZStack {
             // Content
             switch onboardingState {
-            case 0: welcomeSection
+            case 0:
+                welcomeSection
+            case 1:
+                addNameSection
+            case 2:
+                addAgeSection
             default:
                 RoundedRectangle(cornerRadius: 25.0)
                     .foregroundStyle(.green)
@@ -51,7 +58,7 @@ struct OnboardingView: View {
 
 // MARK: COMPONENTS
 extension OnboardingView {
-    /// This is the foreground layer that holds a scrollView
+    /// This is the foreground layer that holds a OnboardingView
     private var bottomButtonLayer: some View {
         Text("Sign in")
             .font(.headline)
@@ -65,36 +72,84 @@ extension OnboardingView {
             }
     }
     
+    /// Foreground layer that holds a OnboardingView
+    /// This snippet shows welcome section
     private var welcomeSection: some View {
-       VStack(spacing: 40) {
-           Spacer()
+        VStack(spacing: 40) {
+            Spacer()
             Image(systemName: "heart.text.square.fill")
-               .resizable()
-               .scaledToFit()
-               .frame(width: 200, height: 200)
-               .foregroundStyle(.white)
-           
-           Text("Find your porpuse.")
-               .font(.largeTitle)
-               .fontWeight(.semibold)
-               .foregroundStyle(.white)
-               .overlay(
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .foregroundStyle(.white)
+            
+            Text("Find your porpuse.")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+                .overlay(
                     Capsule(style: .continuous)
                         .frame(height: 3)
                         .offset(y: 5)
                         .foregroundStyle(.white),
                     alignment: .bottom
-               )
-           
-           Text("A purpose is the driving force behind actions and decisions. It’s the reason we do what we do, providing meaning and direction in our lives.")
-               .font(.body)
-               .fontWeight(.medium)
-               .foregroundStyle(.white)
-               .multilineTextAlignment(.center)
-           
-           Spacer()
-           Spacer()
+                )
+            
+            Text("A purpose is the driving force behind actions and decisions. It’s the reason we do what we do, providing meaning and direction in our lives.")
+                .font(.body)
+                .fontWeight(.medium)
+                .foregroundStyle(.white)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            Spacer()
         }
-       .padding(32)
+        .padding(32)
+    }
+    
+    /// Foreground layer that holds a OnboardingView
+    /// This snippet shows addName section
+    ///
+    private var addNameSection: some View {
+        VStack(spacing: 40) {
+            Spacer()
+            
+            Text("What's your name?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+            
+            TextField("Enter your name...", text: $textFieldName)
+                .font(.headline)
+                .frame(height: 55)
+                .padding(.horizontal)
+                .background(.white)
+                .cornerRadius(8)
+            
+            Spacer()
+            Spacer()
+        }
+        .padding(32)
+    }
+    
+    /// Foreground layer that holds a OnboardingView
+    /// This snippet shows add age section
+    ///
+    private var addAgeSection: some View {
+        VStack(spacing: 40) {
+            Spacer()
+            
+            Text("How old are you?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+            
+            Slider(value: $slideAge, in: 18...100, step: 1)
+                .accentColor(.white)
+            
+            Spacer()
+            Spacer()
+        }
+        .padding(32)
     }
 }
