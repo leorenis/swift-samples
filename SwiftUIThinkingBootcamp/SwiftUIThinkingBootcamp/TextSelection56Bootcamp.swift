f0bfba4a7c69bdfe2b9a3115626c899d0d48103a
leorenis <leorenis@gmail.com>
2025-04-08 08:56:53 -0300

    learn: swift - Text selection

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/TextSelection56Bootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelection56Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo on 08/04/25.
+//
+
+import SwiftUI
+
+struct TextSelection56Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelection56Bootcamp()
+}
