64b6f3b2a7cdda09c1c9766147948235e80fcdf6
leorenis <leorenis@gmail.com>
2025-04-08 08:43:57 -0300

    learn: swift - AsyncImage

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/BackgroundMaterials54Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundMaterials54Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 08/04/25.
+//
+
+import SwiftUI
+
+struct BackgroundMaterials54Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundMaterials54Bootcamp()
+}
