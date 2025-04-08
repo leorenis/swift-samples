bdd49f49619d5c90c10f902103c13c3da70b39f3
leorenis <leorenis@gmail.com>
2025-04-08 09:28:03 -0300

    learn: swift - Text selection

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/TextSelection56Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/TextSelection56Bootcamp.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct TextSelection56Bootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Copy me!")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                Text("On iOS, the person using the app touches and holds on a selectable Text view, which brings up a system menu with menu items appropriate for the current context. These menu items operate on the entire contents of the Text view; the person can’t select a range of text like they can on macOS.")
+                    .textSelection(.enabled)
+            }
             .textSelection(.enabled)
+            .padding(20)
+            .background(.ultraThinMaterial)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(.horizontal)
+        .background(Color("AdaptiveColor"))
+        
+        
+        
     }
 }
 
