cd136f845c7bb15cca0ce2bb890195b1fbabab35
leorenis <leorenis@gmail.com>
2025-04-09 06:10:17 -0300

    learn: swift - Button styles

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ButtonStyles57Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonStyles57Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 08/04/25.
+//
+
+import SwiftUI
+
+struct ButtonStyles57Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonStyles57Bootcamp()
+}
