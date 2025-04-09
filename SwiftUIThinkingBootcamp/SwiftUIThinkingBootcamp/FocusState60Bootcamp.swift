71bf37c13bf4bcc474e7306263120e33d5b46bfd
leorenis <leorenis@gmail.com>
2025-04-09 08:04:53 -0300

    learn: swift - FocusState

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/FocusState60Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  FocusState60Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 09/04/25.
+//
+
+import SwiftUI
+
+struct FocusState60Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FocusState60Bootcamp()
+}
