d5286e282b8b6826261936419eb1e84b25f01900
leorenis <leorenis@gmail.com>
2025-04-09 08:00:08 -0300

    learn: swift - Badges: List, TabView

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Badges59Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  Badges59Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 09/04/25.
+//
+
+import SwiftUI
+
+struct Badges59Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Badges59Bootcamp()
+}
