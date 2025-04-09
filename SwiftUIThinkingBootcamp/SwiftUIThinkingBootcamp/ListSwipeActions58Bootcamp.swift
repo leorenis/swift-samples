97d146622b09e3f9312d11bc41a8a300cbba93a9
leorenis <leorenis@gmail.com>
2025-04-09 06:44:57 -0300

    learn: swift - Swipe action Gesture

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ListSwipeActions58Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ListSwipeActions58Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 09/04/25.
+//
+
+import SwiftUI
+
+struct ListSwipeActions58Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListSwipeActions58Bootcamp()
+}
