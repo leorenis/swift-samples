47e6c85f18c7209a03aa825b49376d25ba36ca64
leorenis <leorenis@gmail.com>
2025-04-11 07:44:10 -0300

    learn: swift - FocusState, .textInputAutocapitalization(.never), focused($biding, equals: hashable).

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/SubmitTextField61Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  SubmitTextField61Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 09/04/25.
+//
+
+import SwiftUI
+
+struct SubmitTextField61Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubmitTextField61Bootcamp()
+}
