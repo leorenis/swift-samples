ccc5ba5bf0d889b6828f195a7f39f89e5bf08c1b
leorenis <leorenis@gmail.com>
2025-04-12 09:53:14 -0300

    learn: NavigationStack toolbar

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Toolbar63Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  Toolbar63Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 12/04/25.
+//
+
+import SwiftUI
+
+struct Toolbar63Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Toolbar63Bootcamp()
+}
