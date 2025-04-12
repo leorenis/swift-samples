4bd271d1b310e4d1d08baa792e86223d706fa652
leorenis <leorenis@gmail.com>
2025-04-12 09:53:22 -0300

    learn: NavigationStack toolbar

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Toolbar63Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Toolbar63Bootcamp.swift
@@ -2,17 +2,20 @@
 //  Toolbar63Bootcamp.swift
 //  SwiftUIThinkingBootcamp
 //
-//  Created by Leo Renis Santos on 12/04/25.
+//  Created by Leo on 12/04/25.
 //
 
 import SwiftUI
 
+/// A data struct to provide struct to practicing FocusState.
 struct Toolbar63Bootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+
+// MARK: PREVIEW
 #Preview {
     Toolbar63Bootcamp()
 }
