b74ce930fde71b739da29e8ef868657dd595cc42
leorenis <leorenis@gmail.com>
2025-04-13 08:32:02 -0300

    learn: Group

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Group66Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Group66Bootcamp.swift
@@ -2,17 +2,20 @@
 //  Group66Bootcamp.swift
 //  SwiftUIThinkingBootcamp
 //
-//  Created by Leo Renis Santos on 13/04/25.
+//  Created by Leo on 13/04/25.
 //
 
 import SwiftUI
 
+/// A data struct to provide struct to practicing Group.
 struct Group66Bootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
+
+// MARK: PREVIEW
 #Preview {
     Group66Bootcamp()
 }
