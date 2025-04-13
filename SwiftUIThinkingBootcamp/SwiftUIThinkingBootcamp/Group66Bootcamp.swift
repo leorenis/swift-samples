68753fb756c9994d11e2ae694d5479f4387b153c
leorenis <leorenis@gmail.com>
2025-04-13 08:31:57 -0300

    learn: Group

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Group66Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  Group66Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 13/04/25.
+//
+
+import SwiftUI
+
+struct Group66Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Group66Bootcamp()
+}
