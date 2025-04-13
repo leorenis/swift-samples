1645e86cb0b98c15ccdd0c0dd229f3081574c07f
leorenis <leorenis@gmail.com>
2025-04-13 08:22:31 -0300

    learn: Resizeble sheet

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/SafeAreaInset65Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  SafeAreaInset65Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 13/04/25.
+//
+
+import SwiftUI
+
+struct SafeAreaInset65Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafeAreaInset65Bootcamp()
+}
