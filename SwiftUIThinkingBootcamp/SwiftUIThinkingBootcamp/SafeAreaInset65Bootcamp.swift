911819fb6019167f9f3c5b647a13908201fe2d56
leorenis <leorenis@gmail.com>
2025-04-13 08:22:50 -0300

    learn: SefeAreaInset

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/SafeAreaInset65Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/SafeAreaInset65Bootcamp.swift
@@ -2,17 +2,52 @@
 //  SafeAreaInset65Bootcamp.swift
 //  SwiftUIThinkingBootcamp
 //
-//  Created by Leo Renis Santos on 13/04/25.
+//  Created by Leo on 13/04/25.
 //
 
 import SwiftUI
 
+/// A data struct to provide struct to practicing SafeAreaInset.
 struct SafeAreaInset65Bootcamp: View {
+    // MARK: PROPERTIES
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(0..<10) { _ in
+                Rectangle()
+                    .frame(height: 300)
+            }
+            .navigationTitle("Safe area insets")
+//            .overlay(
+//                Text("Hello, World!")
+//                    .frame(maxWidth: .infinity)
+//                    .background(Color.yellow)
+//                , alignment: .bottom
+//            )
+            .safeAreaInset(edge: .bottom, alignment: .center, spacing: nil) { // The right way
+                Text("Hello, We're using safeAreaInset!")
+                    .frame(maxWidth: .infinity)
+                    .background(Color.yellow)
+            }
+            .safeAreaInset(edge: .top, alignment: .trailing, spacing: nil) {
+                Text("Hi")
+                    .padding()
+                    .background(Color.indigo)
+                    .clipShape(Circle())
+                    .padding()
+                    .foregroundStyle(.white)
+                    
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .safeAreaInset(edge: .top, alignment: .center, spacing: nil) {
+                Text("Hello, We're using safeAreaInset!")
+                    .frame(maxWidth: .infinity)
+                    .background(Color.yellow.background(ignoresSafeAreaEdges: .bottom))
+            }
+        }
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     SafeAreaInset65Bootcamp()
 }
