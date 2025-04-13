a5f34c550395bf289757cea709aeeba4a01a1a87
leorenis <leorenis@gmail.com>
2025-04-13 06:26:47 -0300

    learn: Resizeble sheet

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ResizebleSheet64Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ResizebleSheet64Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 13/04/25.
+//
+
+import SwiftUI
+
+struct ResizebleSheet64Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResizebleSheet64Bootcamp()
+}
