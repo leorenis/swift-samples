f4bcf25d819eb0c21c3096093db915b149c7b3f0
leorenis <leorenis@gmail.com>
2025-04-14 07:05:01 -0300

    learn: Menu

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Menu68Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  Menu68Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 14/04/25.
+//
+
+import SwiftUI
+
+struct Menu68Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu68Bootcamp()
+}
