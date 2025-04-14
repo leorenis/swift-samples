b5a8efc38ad367b84fc658438ec1d0c2080d94fb
leorenis <leorenis@gmail.com>
2025-04-14 07:40:49 -0300

    learn: Native Popover

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/NativePopover69Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/NativePopover69Bootcamp.swift
@@ -1,18 +1,39 @@
 //
 //  NativePopover69Bootcamp.swift
 //  SwiftUIThinkingBootcamp
+// episode: https://youtu.be/bW7N8ACCc6A?si=-oxX6z43B-2wcqnZ
 //
-//  Created by Leo Renis Santos on 14/04/25.
+//  Created by Leo on 14/04/25.
 //
 
 import SwiftUI
 
+/// The popover modifier let's us show a View popping over an existing View on the screen. This intuitive UI can bring better user experiences to your application
 struct NativePopover69Bootcamp: View {
+    
+    // MARK: PROPERTIES
+    @State private var showPopover: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Background
+            Color.white
+                .ignoresSafeArea(edges: .all)
+            
+            // Foreground
+            Button("Click me") {
+                showPopover.toggle()
+            }
+            .popover(isPresented: $showPopover) {
+                Text("Hello, popover!")
+                    .padding()
+                    .presentationCompactAdaptation(.popover)
+            }
+        }
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     NativePopover69Bootcamp()
 }
