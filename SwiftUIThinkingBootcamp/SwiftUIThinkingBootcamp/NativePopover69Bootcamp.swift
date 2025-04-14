1bf8fdebc78044d08df4d0f1bfaa588dd51f5915
leorenis <leorenis@gmail.com>
2025-04-14 07:40:39 -0300

    learn: Native Popover

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/NativePopover69Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  NativePopover69Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 14/04/25.
+//
+
+import SwiftUI
+
+struct NativePopover69Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NativePopover69Bootcamp()
+}
