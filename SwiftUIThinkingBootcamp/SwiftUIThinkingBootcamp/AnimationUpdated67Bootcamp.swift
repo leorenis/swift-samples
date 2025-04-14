2151a90769223be4c67d318367b84889807adb4a
leorenis <leorenis@gmail.com>
2025-04-14 06:49:53 -0300

    learn: Animation updated

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnimationUpdated67Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnimationUpdated67Bootcamp.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 /// A data struct to provide struct to practicing AnimationUpdated.
 struct AnimationUpdated67Bootcamp: View {
+    
+    // MARK: PROPERTIES
+    @State private var animationActionOne: Bool = false
+    @State private var animationActionTwo: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 40) {
+                Button("Action one") {
+                    animationActionOne.toggle()
+                }
+                Button("Action two") {
+                    animationActionTwo.toggle()
+                }
+                
+               ZStack {
+                   Rectangle()
+                       .frame(width: 100, height: 100)
+                       .frame(maxWidth: .infinity, alignment: animationActionOne ? .leading : .trailing)
+                       .background(Color.green)
+                       .frame(maxHeight: .infinity, alignment: animationActionTwo ? .top: .bottom)
+                       .background(Color.orange)
+                }
+               .frame(maxWidth: .infinity, maxHeight: .infinity)
+               .background(Color.red)
+            }
+        }
+        .animation(.spring(), value: animationActionOne)
+        .animation(.linear(duration: 2), value: animationActionTwo)
+        // Deprecated
+//        .animation(.spring())
     }
 }
 
