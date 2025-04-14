2d35a0c8d16a4247e6578f616486bd62099624cb
leorenis <leorenis@gmail.com>
2025-04-14 05:46:08 -0300

    learn: Animation updated

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnimationUpdated67Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationUpdated67Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 14/04/25.
+//
+
+import SwiftUI
+
+struct AnimationUpdated67Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationUpdated67Bootcamp()
+}
