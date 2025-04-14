27f53fa69fd84372516b98aebce7fa852775dbe9
leorenis <leorenis@gmail.com>
2025-04-14 05:46:19 -0300

    learn: Animation updated

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnimationUpdated67Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnimationUpdated67Bootcamp.swift
@@ -2,17 +2,20 @@
 //  AnimationUpdated67Bootcamp.swift
 //  SwiftUIThinkingBootcamp
 //
-//  Created by Leo Renis Santos on 14/04/25.
+//  Created by Leo on 14/04/25.
 //
 
 import SwiftUI
 
+/// A data struct to provide struct to practicing AnimationUpdated.
 struct AnimationUpdated67Bootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+
+// MARK: PREVIEW
 #Preview {
     AnimationUpdated67Bootcamp()
 }
