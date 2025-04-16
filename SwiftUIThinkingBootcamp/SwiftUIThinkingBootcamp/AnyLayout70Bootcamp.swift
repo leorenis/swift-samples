1b250edb7907be235a790c17354a77fc7f0fdff8
leorenis <leorenis@gmail.com>
2025-04-16 06:01:30 -0300

    learn: AnyLayout basics

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnyLayout70Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnyLayout70Bootcamp.swift
@@ -2,17 +2,20 @@
 //  AnyLayout70Bootcamp.swift
 //  SwiftUIThinkingBootcamp
 //
-//  Created by Leo Renis Santos on 16/04/25.
+//  Created by Leo on 16/04/25.
 //
 
 import SwiftUI
 
+/// A data struct to provide struct to practicing AnyLayout.
 struct AnyLayout70Bootcamp: View {
+    // MARK: PROPERTIES
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     AnyLayout70Bootcamp()
 }
