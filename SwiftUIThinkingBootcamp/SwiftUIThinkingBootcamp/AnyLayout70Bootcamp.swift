a4a645f8dbe9d6314200d71472e4b06ba184672f
leorenis <leorenis@gmail.com>
2025-04-16 06:01:23 -0300

    learn: AnyLayout basics

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnyLayout70Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AnyLayout70Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 16/04/25.
+//
+
+import SwiftUI
+
+struct AnyLayout70Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnyLayout70Bootcamp()
+}
