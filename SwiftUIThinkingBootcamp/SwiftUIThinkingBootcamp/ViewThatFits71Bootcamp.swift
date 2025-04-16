f15c3decefecd5a664cb3e680ce185a96ec137ec
leorenis <leorenis@gmail.com>
2025-04-16 06:46:53 -0300

    learn: ViewThatFits

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ViewThatFits71Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ViewThatFits71Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 16/04/25.
+//
+
+import SwiftUI
+
+struct ViewThatFits71Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewThatFits71Bootcamp()
+}
