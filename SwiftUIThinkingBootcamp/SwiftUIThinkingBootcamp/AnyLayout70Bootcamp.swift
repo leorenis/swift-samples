2b77ab557a1a476a908fd80c0df6c6feb04d80b9
leorenis <leorenis@gmail.com>
2025-04-16 06:37:21 -0300

    learn: AnyLayout using LAyout HStack or VStack according with Environment orientation

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnyLayout70Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnyLayout70Bootcamp.swift
@@ -25,20 +25,25 @@ struct AnyLayout70Bootcamp: View {
             Text("Vertical: \(verticalSizeClass.debugDescription)")
         }
         
-        if horizontalSizeClass == .compact {
-            VStack {
-                Text("Alpha")
-                Text("Beta")
-                Text("Gamma")
-            }
+        let layout: AnyLayout = horizontalSizeClass == .compact ? AnyLayout(VStackLayout()) : AnyLayout(HStackLayout())
+        layout {
+            Text("Alpha")
+            Text("Beta")
+            Text("Gamma")
         }
-        else {
-            HStack {
-                Text("Alpha")
-                Text("Beta")
-                Text("Gamma")
-            }
-        }
+        
+//        if horizontalSizeClass == .compact {
+//            VStack {
+//                
+//            }
+//        }
+//        else {
+//            HStack {
+//                Text("Alpha")
+//                Text("Beta")
+//                Text("Gamma")
+//            }
+//        }
     }
 }
 
