55b41d18ff540bf8bde7f6cc72478e6ab4fbf82e
leorenis <leorenis@gmail.com>
2025-04-16 07:30:12 -0300

    learn: NavigationSplitView

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/NavigationSplitView72Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationSplitView72Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 16/04/25.
+//
+
+import SwiftUI
+
+struct NavigationSplitView72Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationSplitView72Bootcamp()
+}
