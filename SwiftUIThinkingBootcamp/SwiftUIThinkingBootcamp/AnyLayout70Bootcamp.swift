e11ea464cf5ed45e232c7c78e247def5d8de08f8
leorenis <leorenis@gmail.com>
2025-04-16 06:29:12 -0300

    learn: AnyLayout orientation

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnyLayout70Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/AnyLayout70Bootcamp.swift
@@ -2,6 +2,8 @@
 //  AnyLayout70Bootcamp.swift
 //  SwiftUIThinkingBootcamp
 //
+// References: https://useyourloaf.com/blog/size-classes
+// Episode: https://youtu.be/7BAW70amSCA?si=YWX5ol0bNKprVn0M
 //  Created by Leo on 16/04/25.
 //
 
@@ -10,8 +12,33 @@ import SwiftUI
 /// A data struct to provide struct to practicing AnyLayout.
 struct AnyLayout70Bootcamp: View {
     // MARK: PROPERTIES
+    
+    /**
+     * Using theses Environments bellow, Apple gonna tell us if the screen orientation is: Regular or Compact.
+     */
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @Environment(\.verticalSizeClass) private var verticalSizeClass
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Horizontal: \(horizontalSizeClass.debugDescription)")
+            Text("Vertical: \(verticalSizeClass.debugDescription)")
+        }
+        
+        if horizontalSizeClass == .compact {
+            VStack {
+                Text("Alpha")
+                Text("Beta")
+                Text("Gamma")
+            }
+        }
+        else {
+            HStack {
+                Text("Alpha")
+                Text("Beta")
+                Text("Gamma")
+            }
+        }
     }
 }
 
