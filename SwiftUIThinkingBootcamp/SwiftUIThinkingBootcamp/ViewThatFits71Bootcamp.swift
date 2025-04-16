ba1ecfe279597e7c88ca414a75ad85c331eeb565
leorenis <leorenis@gmail.com>
2025-04-16 06:46:59 -0300

    learn: ViewThatFits

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ViewThatFits71Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ViewThatFits71Bootcamp.swift
@@ -1,18 +1,20 @@
 //
 //  ViewThatFits71Bootcamp.swift
 //  SwiftUIThinkingBootcamp
+// Episode: https://youtu.be/oN3Rqo6V6Uc?si=I-pOA4u3l1uz8CvI
 //
-//  Created by Leo Renis Santos on 16/04/25.
+//  Created by Leo on 16/04/25.
 //
 
 import SwiftUI
-
+/// A data struct to provide struct to practicing ViewFits.
 struct ViewThatFits71Bootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     ViewThatFits71Bootcamp()
 }
