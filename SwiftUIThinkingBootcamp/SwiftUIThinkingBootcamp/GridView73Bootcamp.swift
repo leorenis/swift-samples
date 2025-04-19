74b2e1188e8d7bbb4fb40935e2d823c87dc0917a
leorenis <leorenis@gmail.com>
2025-04-19 08:42:43 -0300

    learn: GridView

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/GridView73Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  GridView73Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 19/04/25.
+//
+
+import SwiftUI
+
+struct GridView73Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridView73Bootcamp()
+}
