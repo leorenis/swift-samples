561ed5cee5661267a02b09c7c97369cf3b080f46
leorenis <leorenis@gmail.com>
2025-04-19 08:58:45 -0300

    learn: Grid, GridRow

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/GridView73Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/GridView73Bootcamp.swift
@@ -11,7 +11,28 @@ import SwiftUI
 /// A data struct to provide struct to practicing GridView iOS 16+.
 struct GridView73Bootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Grid {
+            GridRow {
+                cell(value: 1)
+                cell(value: 2)
+                cell(value: 3)
+            }
+            GridRow {
+                cell(value: 4)
+                cell(value: 5)
+            }
+        }
+    }
+    
+    // MARK: FUNCTIONS
+    
+    
+    private func cell(value: Int) -> some View {
+        Text("\(value)")
+            .font(.title)
+            .padding()
+            .foregroundStyle(.white)
+            .background(Color.blue)
     }
 }
 
