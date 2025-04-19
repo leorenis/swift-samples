25a93ad1c280ca0525acdfae78854339c2ecc2aa
leorenis <leorenis@gmail.com>
2025-04-19 08:42:51 -0300

    learn: GridView

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/GridView73Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/GridView73Bootcamp.swift
@@ -1,18 +1,21 @@
 //
 //  GridView73Bootcamp.swift
 //  SwiftUIThinkingBootcamp
+//  Episode: https://youtu.be/LnPMsG0sV50?si=7kqPoMbY-HLpCrNP
 //
-//  Created by Leo Renis Santos on 19/04/25.
+//  Created by Leo on 19/04/25.
 //
 
 import SwiftUI
 
+/// A data struct to provide struct to practicing GridView iOS 16+.
 struct GridView73Bootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     GridView73Bootcamp()
 }
