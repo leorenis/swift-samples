6913b659fe61e40ccd26d6d04e2693cb4cd103b4
leorenis <leorenis@gmail.com>
2025-04-19 11:22:51 -0300

    learn: Content Unavailable View

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ContentUnavailableView74Bootcamp.swift
@@ -0,0 +1,22 @@
+//
+//  ContentUnavailableView74Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//  Episode: https://youtu.be/wfJENH5kfeo?si=XtBn_76ZBLlhuJCh
+//
+//  Created by Leo on 19/04/25.
+//
+
+import SwiftUI
+
+/// A data struct to provide struct to practicing ContentUnavailableView iOS 16+.
+struct ContentUnavailableView74Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+// MARK: PREVIEW
+#Preview {
+    ContentUnavailableView74Bootcamp()
+}
