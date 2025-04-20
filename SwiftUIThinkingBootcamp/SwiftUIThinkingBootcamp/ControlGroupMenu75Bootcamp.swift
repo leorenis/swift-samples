b859e785f0d65f9e263191686d782d9a36b5c5f8
leorenis <leorenis@gmail.com>
2025-04-20 19:16:54 -0300

    learn: Control Group Menu

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ControlGroupMenu75Bootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ControlGroupMenu75Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Leo Renis Santos on 20/04/25.
+//
+
+import SwiftUI
+
+struct ControlGroupMenu75Bootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlGroupMenu75Bootcamp()
+}
