474002778678d9a0659d394f76057b9b6319beea
leorenis <leorenis@gmail.com>
2025-04-20 19:29:22 -0300

    learn: Control Group Menu

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ControlGroupMenu75Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ControlGroupMenu75Bootcamp.swift
@@ -10,7 +10,27 @@ import SwiftUI
 /// A data struct to provide struct to practicing ControlGroupMenu.
 struct ControlGroupMenu75Bootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Menu ("Control Group Menu") {
+            ControlGroup {
+                Button("One") {}
+                Button("Two") {}
+                Button("Tree") {}
+                
+                Menu("How are you?") { // MUST have a label
+                    Button("Good") {}
+                    Button("Bad") {}
+                }
+            }
+            
+            Button("Button menu") {}
+            
+            Menu ("Root level menu") { // MUST have a label
+                Button("Four") {}
+                Button("Five") {}
+                
+            }
+            
+        }
     }
 }
 
