275c46bbaa0766305e0022e0b221b1e8650e2d22
leorenis <leorenis@gmail.com>
2025-04-20 19:17:01 -0300

    learn: Control Group Menu

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ControlGroupMenu75Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/ControlGroupMenu75Bootcamp.swift
@@ -2,17 +2,20 @@
 //  ControlGroupMenu75Bootcamp.swift
 //  SwiftUIThinkingBootcamp
 //
-//  Created by Leo Renis Santos on 20/04/25.
+//  Created by Leo on 20/04/25.
 //
 
 import SwiftUI
 
+/// A data struct to provide struct to practicing ControlGroupMenu.
 struct ControlGroupMenu75Bootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+
+// MARK: PREVIEW
 #Preview {
     ControlGroupMenu75Bootcamp()
 }
