eb8817b68034e54db6faac9fd51856da12eb841e
leorenis <leorenis@gmail.com>
2025-04-21 07:07:12 -0300

    learn: Migrating ObservableObjects to Observables

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Observable76Bootcamp.swift
@@ -0,0 +1,23 @@
+//
+//  Observable76Bootcamp.swift
+//  SwiftUIThinkingBootcamp
+//  Episode: https://youtu.be/hLkTMJ_SFzY?si=Be_YAvY4VZpenBMr
+//  Article: https://developer.apple.com/documentation/swiftui/migrating-from-the-observable-object-protocol-to-the-observable-macro
+//
+//  Created by Leo on 21/04/25.
+//
+
+import SwiftUI
+
+/// A data struct to provide struct to practicing Observable.
+struct Observable76Bootcamp: View {
+    var body: some View {
+        
+    }
+}
+
+
+// MARK: PREVIEW
+#Preview {
+    Observable76Bootcamp()
+}
