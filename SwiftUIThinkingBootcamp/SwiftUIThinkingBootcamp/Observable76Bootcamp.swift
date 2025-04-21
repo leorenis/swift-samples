baa5862ccee6ef93c6dd1778905926ff11364511
leorenis <leorenis@gmail.com>
2025-04-21 07:42:08 -0300

    learn: Migrating ObservableObjects to Observables

--- SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Observable76Bootcamp.swift
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/Observable76Bootcamp.swift
@@ -9,10 +9,38 @@
 
 import SwiftUI
 
+class ObservableViewModel: ObservableObject {
+    @Published var title: String = "Morning"
+}
+
 /// A data struct to provide struct to practicing Observable.
 struct Observable76Bootcamp: View {
+    
+    // MARK: PROPERTIES
+    @StateObject private var viewModel: ObservableViewModel = ObservableViewModel()
+    
+    /// View Body: block  responsable for acts as entry point in this view.
     var body: some View {
-        
+        VStack(spacing: 20) {
+            Button(viewModel.title) {
+                viewModel.title = "Morning..."
+            }
+            
+            GreetingsObserbableView(viewModel: viewModel)
+        }
+    }
+}
+
+/// A data struct to provide struct to practicing Observable.
+struct GreetingsObserbableView: View {
+    // MARK: PROPERTIES
+    @ObservedObject var viewModel: ObservableViewModel
+    
+    /// View Body: block  responsable for acts as entry point in this view.
+    var body: some View {
+        Button(viewModel.title) {
+            viewModel.title = "Morning greeting"
+        }
     }
 }
 
