039e7a87b11ff74d54cdf3412c333cb21306e69e
leorenis <leorenis@gmail.com>
2025-04-22 09:08:30 -0300

    learn: First iOS App. TodoList -TaskModel: Refactoring to became id more flexible

--- TodoList/TodoList/Models/TaskModel.swift
+++ TodoList/TodoList/Models/TaskModel.swift
@@ -7,8 +7,36 @@
 
 import Foundation
 
+/// Imutable struct to represents TaskModel
 struct TaskModel: Identifiable {
-    let id: String = UUID().uuidString
+    let id: String
     let title: String
     let isCompleted: Bool
+    
+    /// Init an Imutable struct TaskModel.
+    ///
+    /// When TaskModel receive a id, creates a new TaskModel preserving the id provided.
+    /// Otherwise, creates a new Task model with a new UUID as string.
+    ///
+    ///```
+    ///init(id: String = UUID().uuidString, title: String, isCompleted: Bool)
+    ///```
+    ///
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    /// Performs toggle for isCompleted property.
+    ///
+    ///```
+    ///toggleCompletion() ->TaskModel(id: id, title: title, isCompleted: !isCompleted)
+    ///```
+    /// - Returns: Returns a new TaskModel changing the isCompleted property.
+    /// - Complexity: O(1) on average, only create a new TaskModel with the same data, only toggle isCompleted property.
+    ///
+    mutating func toggleCompletion() -> TaskModel {
+        return TaskModel(id: id, title: title, isCompleted: !isCompleted)
+    }
 }
