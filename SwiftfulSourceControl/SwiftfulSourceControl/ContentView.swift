3717df43bbe88e9f36c0b76c97629796215605c0
leorenis <leorenis@gmail.com>
2025-04-28 08:37:40 -0300

    course: Source control - First commit

--- SwiftfulSourceControl/SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/SwiftfulSourceControl/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+        VStack(spacing: 16) {
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Git Source Control in Swift!")
+                .foregroundStyle(.primary)
         }
         .padding()
     }
