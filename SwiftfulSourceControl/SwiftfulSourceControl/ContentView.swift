c59cd3c85a6782bfc87f7d4b76dd14767d377102
leorenis <leorenis@gmail.com>
2025-04-28 09:12:57 -0300

    course: Source control. Commit messages suggestions

--- SwiftfulSourceControl/SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by Leo Renis Santos on 28/04/25.
 //
 
+/**
+ * Sugestion: Commit messages.
+ *
+ *  NEW FEATURE:
+ *  [Feature] Desciption of the feature.
+ *
+ *  BUG IN PRODUCTION:
+ *  [Patch] Description of patch
+ *         Obs: Personal, preffer [Fix].
+ *
+ *  BUG NOT IN PRODUCTION:
+ *  [Bug] Description of the Bug.
+ *
+ *  MUNDANE TASKS:
+ *  [Clean] Description of changes.
+ *
+ *  RELEASE:
+ *  [Release] Description of release.
+ */
+
 import SwiftUI
 
 struct ContentView: View {
