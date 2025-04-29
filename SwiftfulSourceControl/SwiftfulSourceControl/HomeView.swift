f960326ce2dcb680ff80970c375c583cb9bc579f
leorenis <leorenis@gmail.com>
2025-04-29 06:36:53 -0300

    [Feature] Add Home View.

---
+++ SwiftfulSourceControl/SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Leo Renis Santos on 29/04/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
