705bb9b0d9e0092d58e7667819152dabe89cd7cd
leorenis <leorenis@gmail.com>
2025-05-05 08:47:15 -0300

    [Map] Setup project Map.

---
+++ SwiftfulMapApp/SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by Leo on 05/05/25.
+//
+
+import Foundation
---
+++ SwiftfulMapApp/SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Leo Renis Santos on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
