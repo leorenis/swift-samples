b7f4c440d11441720823057e07d542d4633eddcd
leorenis <leorenis@gmail.com>
2025-05-06 08:13:14 -0300

    [Map] Refactoring Locations feature.

--- SwiftfulMapApp/SwiftfulMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftfulMapApp/SwiftfulMapApp/ViewModels/LocationsViewModel.swift
@@ -2,7 +2,18 @@
 //  LocationsViewModel.swift
 //  SwiftfulMapApp
 //
-//  Created by Leo Renis Santos on 06/05/25.
+//  Created by Leo on 06/05/25.
 //
 
 import Foundation
+
+/// A class to provide data access for our tasks.
+class LocationsViewModel: ObservableObject {
+    @Published var locations: [Location] = []
+    
+    /// Creates initialization to Locations from LocationsDataService.
+    init() {
+        let locations = LocationsDataService.locations
+        self.locations = locations
+    }
+}
--- SwiftfulMapApp/SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/SwiftfulMapApp/Views/LocationsView.swift
@@ -7,21 +7,11 @@
 
 import SwiftUI
 
-class LocationsViewModel: ObservableObject {
-    @Published var locations: [Location] = []
-    
-    /// Creates initialization to Locations from LocationsDataService.
-    init() {
-        let locations = LocationsDataService.locations
-        self.locations = locations
-    }
-}
-
 /// A data struct to provide struct to LocationsView.
 struct LocationsView: View {
     
     // MARK: PROPERTIES
-    @StateObject private var vm = LocationsViewModel()
+    @EnvironmentObject private var vm: LocationsViewModel
     
     /// View Body: block  responsable for acts as entry point in this view.
     var body: some View {
@@ -39,4 +29,5 @@ struct LocationsView: View {
 // MARK: PREVIEW
 #Preview {
     LocationsView()
+        .environmentObject(LocationsViewModel())
 }
