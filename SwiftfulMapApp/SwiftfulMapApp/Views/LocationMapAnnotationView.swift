fd2f943184b5e0192a4be66688afab10ee5dcd2a
leorenis <leorenis@gmail.com>
2025-05-11 11:40:39 -0300

    feature: LocationMapAnnotationView

--- SwiftfulMapApp/SwiftfulMapApp/Views/LocationMapAnnotationView.swift
+++ SwiftfulMapApp/SwiftfulMapApp/Views/LocationMapAnnotationView.swift
@@ -10,15 +10,39 @@ import SwiftUI
 struct LocationMapAnnotationView: View {
     
     // MARK: PROPERTIES
+    let accentColor = Color.accentColor
     
     /// View Body: block  responsable for acts as entry point in this view.
     var body: some View {
-        Text("Hello, World!")
+        VStack (spacing: 0) {
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 32, height: 32)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(8)
+                .background(accentColor)
+                .clipShape(Circle())
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 12, height: 12)
+                .foregroundColor(accentColor)
+                .rotationEffect(.degrees(180))
+                .offset(y: -3)
+                .padding(.bottom, 8)
+        }
     }
 }
 
 // MARK: PREVIEW
 #Preview {
-    LocationMapAnnotationView()
-        .environmentObject(LocationsViewModel())
+    ZStack {
+        Color.black
+            .ignoresSafeArea()
+        LocationMapAnnotationView()
+            .environmentObject(LocationsViewModel())
+    }
 }
