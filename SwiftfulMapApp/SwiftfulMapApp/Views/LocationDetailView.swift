70f55b6049b26a719278283d73bb359279d883c4
leorenis <leorenis@gmail.com>
2025-05-13 08:06:52 -0300

    feature: LocationDetailView

--- SwiftfulMapApp/SwiftfulMapApp/Views/LocationDetailView.swift
+++ SwiftfulMapApp/SwiftfulMapApp/Views/LocationDetailView.swift
@@ -2,17 +2,39 @@
 //  LocationDetailView.swift
 //  SwiftfulMapApp
 //
-//  Created by Leo Renis Santos on 13/05/25.
+//  Created by Leo on 13/05/25.
 //
 
 import SwiftUI
 
+/// A data struct to provide  LocationDetailView.
 struct LocationDetailView: View {
+    
+    // MARK: PROPERTIES
+    let location: Location
+    
+    /// View Body: block  responsable for acts as entry point in this view.
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                TabView {
+                    ForEach(location.imageNames, id: \.self) {
+                        Image($0)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: UIScreen.main.bounds.width)
+                            .clipped()
+                    }
+                }
+                .frame(height: 500)
+                .tabViewStyle(PageTabViewStyle())
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
+// MARK: PREVIEW
 #Preview {
-    LocationDetailView()
+    LocationDetailView(location: LocationsDataService.fallbackLocation)
 }
