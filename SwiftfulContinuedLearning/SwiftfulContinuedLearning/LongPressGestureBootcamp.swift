a984779046129ec11660fb0ad905d54daba004a5
leorenis <leorenis@gmail.com>
2025-05-19 08:46:13 -0300

    fix: continued learning: Long press gesture. real world sample

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/LongPressGestureBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/LongPressGestureBootcamp.swift
@@ -14,6 +14,15 @@ struct LongPressGestureBootcamp: View {
     
     // MARK: BODY
     var body: some View {
+        // simpleLongPressGestureView
+        realExempleLongPressGestureView
+    }
+}
+
+// MARK: EXTENSIONS
+extension LongPressGestureBootcamp {
+    /// A tiny simple exemple to long pressed gesture using minimumDuration, maximumDistance.
+    private var simpleLongPressGestureView: some View {
         Text(isCompleted ? "Completed!" :"Not completed!")
             .padding()
             .padding(.horizontal, 12)
@@ -25,6 +34,33 @@ struct LongPressGestureBootcamp: View {
             .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) {
                 isCompleted.toggle()
             }
+    }
+    
+    /// A real exemple to long pressed gesture using...
+    private var realExempleLongPressGestureView: some View {
+        VStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(maxWidth: 10)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("click me!")
+                    .colorInvert()
+                    .padding()
+                    .background(Color(.systemCyan))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                
+                Text("Reset")
+                    .colorInvert()
+                    .padding()
+                    .background(Color(.systemPink))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            }
+                
+        }
     }
 }
 
