465cfee4db13fda6fd8640cd247c088623a9191f
leorenis <leorenis@gmail.com>
2025-05-20 07:28:57 -0300

    fix: continued learning: MagnificationGesture.

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/MagnificationGestureBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  MagnificationGestureBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 20/05/25.
+//
+
+import SwiftUI
+
+struct MagnificationGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MagnificationGestureBootcamp()
+}
