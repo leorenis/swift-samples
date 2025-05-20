ac8df9a51741530748f1d0c76ce22fc52495f56e
leorenis <leorenis@gmail.com>
2025-05-20 08:56:20 -0300

    fix: continued learning: Rotation gesture

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/RotationGestureBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  RotationGestureBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 20/05/25.
+//
+
+import SwiftUI
+
+struct RotationGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotationGestureBootcamp()
+}
