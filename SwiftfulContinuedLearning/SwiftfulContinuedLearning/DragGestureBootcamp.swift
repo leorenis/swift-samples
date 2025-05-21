7d0a3da893c27da6c0443847a3cf8b90f241fbf1
leorenis <leorenis@gmail.com>
2025-05-21 08:51:51 -0300

    fix: continued learning: Drag gesture: View

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DragGestureBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  DragGestureBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 21/05/25.
+//
+
+import SwiftUI
+
+struct DragGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragGestureBootcamp()
+}
