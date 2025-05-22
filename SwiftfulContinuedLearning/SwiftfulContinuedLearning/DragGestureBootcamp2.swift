e1ea8d2159f233f65e6b1c98c63cb7ae6053f548
leorenis <leorenis@gmail.com>
2025-05-22 09:40:04 -0300

    fix: continued learning: Drag gesture

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/DragGestureBootcamp2.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DragGestureBootcamp2.swift
@@ -6,13 +6,62 @@
 //
 
 import SwiftUI
-
+/// Drag Gesture training
 struct DragGestureBootcamp2: View {
+    // MARK: PROPERTIES
+    
+    // MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+            DragSignUpView()
+        }
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     DragGestureBootcamp2()
+}
+
+// MARK: SUBVIEWS
+struct DragSignUpView: View {
+    // MARK: PROPERTIES
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: "chevron.up")
+                .padding(.top)
+            
+            Text("Sign up")
+                .font(.headline)
+                .fontWeight(.semibold)
+            
+            Image(systemName: "flame.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+            
+            Text("This is the description of the sign up button in the app icon view controller of the Sign Up app template. This is my favorite course in the app.")
+                .multilineTextAlignment(.center)
+            
+            Button {
+                
+            } label: {
+                Text("CREATE AN ACCOUNT")
+                    .foregroundStyle(.white)
+                    .font(.headline)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                
+            }
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 30))
+    }
 }
