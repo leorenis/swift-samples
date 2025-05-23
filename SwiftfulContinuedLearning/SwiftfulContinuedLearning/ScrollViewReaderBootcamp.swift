659581351cfd80c9fd9f2efc764065d2a3f601d2
leorenis <leorenis@gmail.com>
2025-05-23 08:08:08 -0300

    fix: continued learning: Scroll view reader

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/ScrollViewReaderBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewReaderBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 23/05/25.
+//
+
+import SwiftUI
+
+struct ScrollViewReaderBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewReaderBootcamp()
+}
