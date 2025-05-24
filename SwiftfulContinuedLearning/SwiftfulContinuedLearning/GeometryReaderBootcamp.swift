d81c718a2414ec6e6d66e45d45e26c33b90e2596
leorenis <leorenis@gmail.com>
2025-05-24 09:56:48 -0300

    learn: continued learning: Geometry Reader

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/GeometryReaderBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/GeometryReaderBootcamp.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 /// Geometry Reader struct. Try not to use, but when really neet, go ahead and use it. Nick's comment.
+/// GeometryReader is expense, it runs tasks to calculate geometry screen. Use only when you are certanly that you need. If you not sure, use UIScreen.main.bounds.width
 struct GeometryReaderBootcamp: View {
     //MARK: PROPERTIES
     
     //MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            Rectangle().fill(Color.red)
+            Rectangle().fill(Color.blue)
+        }
+        .ignoresSafeArea()
     }
 }
 
