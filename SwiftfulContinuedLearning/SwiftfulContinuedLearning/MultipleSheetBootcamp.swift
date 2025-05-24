2c1594fbe30af14ef78a84db559f1435f06c10cf
leorenis <leorenis@gmail.com>
2025-05-24 12:01:43 -0300

    learn: continued learning: manage Multiples Sheets

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/MultipleSheetBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/MultipleSheetBootcamp.swift
@@ -2,17 +2,21 @@
 //  MultipleSheetBootcamp.swift
 //  SwiftfulContinuedLearning
 //
-//  Created by Leo Renis Santos on 24/05/25.
+//  Created by Leo on 24/05/25.
 //
 
 import SwiftUI
-
+/// Tricks
+/// How to: Multiple sheets in a SwiftUI View
 struct MultipleSheetBootcamp: View {
+    // MARK: PROPERTIES
+    
+    // MARK: BODY
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
-
+// MARK: PREVIEW
 #Preview {
     MultipleSheetBootcamp()
 }
