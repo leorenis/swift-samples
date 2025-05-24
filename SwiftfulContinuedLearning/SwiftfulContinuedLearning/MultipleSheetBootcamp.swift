533cd5cae6ad47909566db546e6b44dbcfc42ba3
leorenis <leorenis@gmail.com>
2025-05-24 12:01:38 -0300

    learn: continued learning: manage Multiples Sheets

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/MultipleSheetBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  MultipleSheetBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 24/05/25.
+//
+
+import SwiftUI
+
+struct MultipleSheetBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultipleSheetBootcamp()
+}
