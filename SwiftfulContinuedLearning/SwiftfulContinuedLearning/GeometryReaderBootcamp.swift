7a1f863ab200d28250dcf2430ca60ba464b9b27a
leorenis <leorenis@gmail.com>
2025-05-24 09:44:51 -0300

    learn: continued learning: Geometry Reader

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/GeometryReaderBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/GeometryReaderBootcamp.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 
+/// Geometry Reader struct. Try not to use, but when really neet, go ahead and use it. Nick's comment.
 struct GeometryReaderBootcamp: View {
+    //MARK: PROPERTIES
+    
+    //MARK: BODY
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     GeometryReaderBootcamp()
 }
