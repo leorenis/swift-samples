cd519f1ede30c229d929b420a56d5ce4fff5c0e2
leorenis <leorenis@gmail.com>
2025-05-24 09:42:23 -0300

    learn: continued learning: Geometry Reader

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/GeometryReaderBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryReaderBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 24/05/25.
+//
+
+import SwiftUI
+
+struct GeometryReaderBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryReaderBootcamp()
+}
