0172aa573804186c1fc9d8cae94074df99caade7
leorenis <leorenis@gmail.com>
2025-05-24 12:27:18 -0300

    learn: continued learning: manage Multiples Sheets. Proposital BUG: in content: {...}

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/MultipleSheetBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/MultipleSheetBootcamp.swift
@@ -10,13 +10,48 @@ import SwiftUI
 /// How to: Multiple sheets in a SwiftUI View
 struct MultipleSheetBootcamp: View {
     // MARK: PROPERTIES
+    @State private var selectedModel: RandomModel = RandomModel(title: "Starting title")
+    @State private var showSheet: Bool = false
     
     // MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Button("Sheet 1") {
+                selectedModel = RandomModel(title: "Title sheet One")
+                showSheet.toggle()
+            }
+            
+            Button("Sheet 2") {
+                selectedModel = RandomModel(title: "Title sheet Two")
+                showSheet.toggle()
+            }
+        }
+        .sheet(isPresented: $showSheet, content: {
+            NextScreenSheetView(selectedModel: selectedModel)
+        })
     }
 }
 // MARK: PREVIEW
 #Preview {
     MultipleSheetBootcamp()
+}
+
+
+// MARK: MODELS
+struct RandomModel: Identifiable {
+    let id: String = UUID().uuidString
+    let title: String
+}
+
+// MARK: VIEWS
+struct NextScreenSheetView: View {
+    // MARK: PROPERTIES
+    let selectedModel: RandomModel
+    var body: some View {
+        ZStack {
+            Text("\(selectedModel.title)")
+                .font(.title)
+                .foregroundStyle(.secondary)
+        }
+    }
 }
