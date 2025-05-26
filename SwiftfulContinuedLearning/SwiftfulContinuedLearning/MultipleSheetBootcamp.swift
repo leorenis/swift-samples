794e6729bfe814dcb172fa0a374f07e961ff5be5
leorenis <leorenis@gmail.com>
2025-05-26 08:11:20 -0300

    learn: continued learning: manage Multiples Sheets. Full

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/MultipleSheetBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/MultipleSheetBootcamp.swift
@@ -1,7 +1,7 @@
 //
 //  MultipleSheetBootcamp.swift
 //  SwiftfulContinuedLearning
-//
+//  Episode: https://youtu.be/8rCtYoG9JIM?si=kPd6yhbegOiBIO3E
 //  Created by Leo on 24/05/25.
 //
 
@@ -10,9 +10,9 @@
  * The content is created, before selectedModel receive the current value, in this case a new RandomModel(...)
  *
  *  Solution:
- *  1. use a binding;
- *  2. use multiple .sheets;
- *  3. use $item approach.
+ *  1. use a binding; https://github.com/leorenis/swift-samples/commit/05c4c2c22933cfcf42189362022dc16a522f7940
+ *  2. use multiple .sheets https://github.com/leorenis/swift-samples/commit/023f09da8520f62240b0c765133bb87fad41be55;
+ *  3. use $item approach https://github.com/leorenis/swift-samples/commit/a06db6c0dc44020b750ba50f51cb4762c3fbdd8f.
  */
 
 import SwiftUI
