c128a82c57b2b3aa0e19b1fb7e0e41294f684136
leorenis <leorenis@gmail.com>
2025-05-26 08:25:50 -0300

    learn: continued learning: Mask

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/MaskBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  MaskBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo on 26/05/25.
+//
+
+import SwiftUI
+
+/// Mask struct to learning how to mask in SwiftUI.
+struct MaskBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: PREVIEW
+#Preview {
+    MaskBootcamp()
+}
