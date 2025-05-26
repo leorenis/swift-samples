139470d2513c3676ddfb6703e1955372bfbcc3db
leorenis <leorenis@gmail.com>
2025-05-26 08:37:18 -0300

    learn: continued learning: Mask: StarsView

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/MaskBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/MaskBootcamp.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 /// Mask struct to learning how to mask in SwiftUI.
 struct MaskBootcamp: View {
+    // MARK: PROPERTIES
+    @State private var rating: Int = 3
+    
+    // MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            starsView
+        }
+    }
+}
+
+// MARK: EXTENSIONS
+extension MaskBootcamp {
+    /// Gets a view contain a list of starts as a component.
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(rating >= index ? Color.yellow : Color.gray)
+                    .onTapGesture {
+                        rating = index
+                    }
+            }
+        }
     }
 }
 
