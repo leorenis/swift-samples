7c87e0396cedc6a7c639a3f7394fc54c212dd0e5
leorenis <leorenis@gmail.com>
2025-05-29 08:51:34 -0300

    learn: continued learning: Mask: Haptics

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/HapticsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  HapticsBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 29/05/25.
+//
+
+import SwiftUI
+
+struct HapticsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HapticsBootcamp()
+}
