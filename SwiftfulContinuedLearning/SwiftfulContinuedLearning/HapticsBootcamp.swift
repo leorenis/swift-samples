63c8c029c11b5383cf19d7ac82c8c1fd4bc4ec4e
leorenis <leorenis@gmail.com>
2025-05-29 08:56:04 -0300

    learn: continued learning: Mask: Haptics Singleton

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/HapticsBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/HapticsBootcamp.swift
@@ -7,6 +7,40 @@
 
 import SwiftUI
 
+/// Class v is a Singleton responsible for manager haptics in our App.
+class HapticsManager {
+    
+    private init() {} // As a singleton, its must not be initialized out of this class.
+    
+    // MARK: PROPERTIES
+    static let instance = HapticsManager() // Singleton
+    
+    // MARK: ENUM
+    
+    // MARK: FUNCTIONS
+    
+    /// Perform notification in a singleton instance.
+    ///
+    /// Performs notification in a singleton instance of HapticsManager.
+    ///
+    /// ```
+    ///     notification(sound: .tada)
+    /// ```
+    func notification() {
+        
+    }
+    
+    /// Perform impact in a singleton instance.
+    ///
+    /// Performs impact in a singleton instance of HapticsManager.
+    ///
+    /// ```
+    ///     impact(sound: .tada)
+    /// ```
+    func impact() {
+        
+    }
+}
 /// Struct used to training how to create Haptics vibrations.
 struct HapticsBootcamp: View {
     // MARK: BODY
