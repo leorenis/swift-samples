bd677ebde53c1a1daedf9247b2ca77793ac1416e
leorenis <leorenis@gmail.com>
2025-05-30 08:49:48 -0300

    learn: continued learning: LocalNotification

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/LocalNotificationBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/LocalNotificationBootcamp.swift
@@ -2,17 +2,19 @@
 //  LocalNotificationBootcamp.swift
 //  SwiftfulContinuedLearning
 //
-//  Created by Leo Renis Santos on 30/05/25.
+//  Created by Leo on 30/05/25.
 //
 
 import SwiftUI
 
 struct LocalNotificationBootcamp: View {
+    // MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, Local Notification!")
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     LocalNotificationBootcamp()
 }
