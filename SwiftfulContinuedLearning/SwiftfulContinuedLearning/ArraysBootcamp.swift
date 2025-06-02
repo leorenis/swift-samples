f5112068b84383148297ae5d5494fa71d49a2744
leorenis <leorenis@gmail.com>
2025-06-02 08:31:25 -0300

    learn: continued learning: Sort, Filter, Map

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/ArraysBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/ArraysBootcamp.swift
@@ -2,17 +2,20 @@
 //  ArraysBootcamp.swift
 //  SwiftfulContinuedLearning
 //
-//  Created by Leo Renis Santos on 02/06/25.
+//  Created by Leo on 02/06/25.
 //
 
 import SwiftUI
 
+// Struct to training more about Arrays
 struct ArraysBootcamp: View {
+    // MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, Arrays!")
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     ArraysBootcamp()
 }
