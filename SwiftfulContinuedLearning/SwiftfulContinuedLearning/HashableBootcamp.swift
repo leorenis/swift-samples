2efbc68016293a29d5258e15aa1410f297c35ec4
leorenis <leorenis@gmail.com>
2025-06-02 08:20:39 -0300

    learn: continued learning: Hashable, items on screen

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/HashableBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/HashableBootcamp.swift
@@ -10,18 +10,33 @@ import SwiftUI
 // Struct to training hashable.
 struct HashableBootcamp: View {
     // MARK: PROPERTIES
-    let data: [String] = ["ONE", "TWO", "THREE", "FOUR", "FIVE"]
+    let data: [MyCustomModel] = [
+        MyCustomModel(title: "ONE"),
+        MyCustomModel(title: "TWO"),
+        MyCustomModel(title: "THREE"),
+        MyCustomModel(title: "FOUR"),
+        MyCustomModel(title: "FIVE"),
+    ]
     
     // MARK: BODY
     var body: some View {
         ScrollView {
             VStack(spacing: 24) {
                 ForEach(data, id: \.self) { item in
-                    Text(item)
+                    Text(item.title)
                         .font(.headline)
                 }
             }
         }
+    }
+}
+
+// MARK: STRUCTS
+struct MyCustomModel: Hashable {
+    let title: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
     }
 }
 
