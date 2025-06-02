3d181f4c3cd0a2e309df0d10c0243622cb3913e3
leorenis <leorenis@gmail.com>
2025-06-02 07:27:56 -0300

    learn: continued learning: Hashable

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/HashableBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/HashableBootcamp.swift
@@ -2,17 +2,20 @@
 //  HashableBootcamp.swift
 //  SwiftfulContinuedLearning
 //
-//  Created by Leo Renis Santos on 02/06/25.
+//  Created by Leo on 02/06/25.
 //
 
 import SwiftUI
 
+// Struct to training hashable.
 struct HashableBootcamp: View {
+    // MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, Hashable!")
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     HashableBootcamp()
 }
