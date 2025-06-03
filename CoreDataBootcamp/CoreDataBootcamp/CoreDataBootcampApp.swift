642c851fb3edc569901d5fdad75ea60f624b1737
leorenis <leorenis@gmail.com>
2025-06-03 08:22:46 -0300

    learn: continued learning: Core Data

---
+++ CoreDataBootcamp/CoreDataBootcamp/CoreDataBootcampApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataBootcampApp.swift
+//  CoreDataBootcamp
+//
+//  Created by Leo Renis Santos on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBootcampApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
