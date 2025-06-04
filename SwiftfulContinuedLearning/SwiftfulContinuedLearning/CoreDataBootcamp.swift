13dd129c8d72c4885cb9b0ddccb930e0b4075678
leorenis <leorenis@gmail.com>
2025-06-04 08:48:51 -0300

    learn: continued learning: Core Data Save items

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CoreDataBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 04/06/25.
+//
+
+import SwiftUI
+
+struct CoreDataBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataBootcamp()
+}
