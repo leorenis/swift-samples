1ffb6a03d1ec254a408cb53dd530c3d0113dc919
leorenis <leorenis@gmail.com>
2025-06-04 09:20:03 -0300

    learn: continued learning: Core Data

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/CoreDataBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CoreDataBootcamp.swift
@@ -6,8 +6,30 @@
 //
 
 import SwiftUI
+import CoreData
+
+// View - UI
+// Model - data point
+// ViewModel - manages the data for a view
+
+// MARK: CLASSES
+class CoreDataViewModel: ObservableObject {
+    let container: NSPersistentContainer
+    init() {
+        container = NSPersistentContainer(name: "")
+        container.loadPersistentStores { (description, error) in
+            if let error = error {
+                print("Error loading core data. \(error)")
+            }
+        }
+    }
+}
 
 struct CoreDataBootcamp: View {
+    // MARK: PROPERTIES
+    @StateObject private var vm = CoreDataViewModel()
+    
+    // MARK: BODY
     var body: some View {
         
     }
