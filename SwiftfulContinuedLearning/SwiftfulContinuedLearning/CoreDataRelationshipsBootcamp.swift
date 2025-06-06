6164e8d302b6f602a2ede8d58cebec73dc420aac
leorenis <leorenis@gmail.com>
2025-06-06 07:41:46 -0300

    learn: continued learning: Core Data - Relationships

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CoreDataRelationshipsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataRelationshipsBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 06/06/25.
+//
+
+import SwiftUI
+
+struct CoreDataRelationshipsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataRelationshipsBootcamp()
+}
