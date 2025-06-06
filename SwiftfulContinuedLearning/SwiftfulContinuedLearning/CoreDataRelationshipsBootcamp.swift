bc9641467e6f7f95a50310b1bc81d940ac346440
leorenis <leorenis@gmail.com>
2025-06-06 08:53:40 -0300

    learn: continued learning: Core Data - Relationships

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/CoreDataRelationshipsBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CoreDataRelationshipsBootcamp.swift
@@ -6,9 +6,42 @@
 //
 
 import SwiftUI
+import CoreData
 
+//MARK: CLASSES
+class CoreDataManager {
+    static let instance = CoreDataManager()
+    let container: NSPersistentContainer
+    let context: NSManagedObjectContext
+    
+    private init() {
+        container = NSPersistentContainer(name: "")
+        container.loadPersistentStores { (description, error) in
+            if let error = error {
+                print("Error loading core data \(error)")
+            }
+        }
+        context = container.viewContext
+    }
+    
+    fileprivate func save() {
+        do {
+            try context.save()
+        } catch let error {
+            print("Error saving Core Data \(error)")
+        }
+    }
+}
+
+class CoreDataRelationshipViewModel: ObservableObject {
+    
+}
+
+// MARK: STRUCTS
 /// Struct to learning relationships in core data.
 struct CoreDataRelationshipsBootcamp: View {
+    // MARK: PROPERTIES
+    @StateObject private var vm = CoreDataRelationshipViewModel()
     
     // MARK: BODY
     var body: some View {
