dd4e94d7ef2c242b12909a9744ed83b6262d0b1e
leorenis <leorenis@gmail.com>
2025-06-09 09:02:34 -0300

    learn: continued learning: Core Data - Relationships Business

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/CoreDataRelationshipsBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CoreDataRelationshipsBootcamp.swift
@@ -70,9 +70,20 @@ class CoreDataRelationshipViewModel: ObservableObject {
         save()
     }
     
+    fileprivate func addDepartment() {
+        let newDepartment = DepartmentEntity(context: manager.context)
+        newDepartment.name = "Engineering"
+        newDepartment.businesses = [businesses[0]]
+        save()
+        
+    }
+    
     fileprivate func save() {
-        manager.save()
-        fetchBusinesses()
+        businesses.removeAll()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.manager.save()
+            self.fetchBusinesses()
+        }
     }
     
     fileprivate func fetchBusinesses() {
@@ -97,7 +108,8 @@ struct CoreDataRelationshipsBootcamp: View {
             ScrollView {
                 VStack(spacing: 12) {
                     Button(action: {
-                        vm.addBusiness()
+                        //vm.addBusiness()
+                        vm.addDepartment()
                     }, label: {
                         Text("Save")
                             .font(.headline)
@@ -149,6 +161,38 @@ struct BusinessView: View {
         .padding()
         .frame(maxWidth: 300, alignment: .leading)
         .background(Color.gray.opacity(0.5))
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .shadow(radius: 10)
+    }
+}
+
+/// Struct responsible to show DepartmentView
+struct DepartmentView: View {
+    let entity: DepartmentEntity
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20, content: {
+            Text("Name: \(entity.name ?? "No name")")
+                .bold()
+            
+            if let businesses = entity.businesses?.allObjects as? [BusinessEntity] {
+                Text("Businesses:")
+                    .bold()
+                ForEach(businesses) { business in
+                    Text(business.name ?? "No name")
+                }
+            }
+            
+            if let employees = entity.employees?.allObjects as? [EmployeeEntity] {
+                Text("Employees: ")
+                    .bold()
+                ForEach(employees) { employee in
+                    Text(employee.name ?? "no name")
+                }
+            }
+        })
+        .padding()
+        .frame(maxWidth: 300, alignment: .leading)
+        .background(Color.green.opacity(0.5))
         .clipShape(RoundedRectangle(cornerRadius: 20))
         .shadow(radius: 10)
     }
