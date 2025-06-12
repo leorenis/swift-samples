f814c804a2e1b54d2617192e99cb0ee446ca3974
leorenis <leorenis@gmail.com>
2025-06-12 08:15:57 -0300

    learn: continued learning: Multi-threading with background threads and Queues setup

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/BackgroundThreadBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/BackgroundThreadBootcamp.swift
@@ -6,8 +6,17 @@
 //
 
 import SwiftUI
+// MARK: VIEWMODEL
+class BackgroundThreadViewModel: ObservableObject {
+    
+}
 
+// MARK: STRUCTS
 struct BackgroundThreadBootcamp: View {
+    // MARK: PROPERTIES
+    @StateObject private var vm = BackgroundThreadViewModel()
+    
+    // MARK: BODY
     var body: some View {
         Text("Hello, BG Threads!")
     }
