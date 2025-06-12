84163faab7c33c25913f4bee60f7f1c85310ca84
leorenis <leorenis@gmail.com>
2025-06-12 09:25:39 -0300

    learn: continued learning: Multi-threading with background threads and DispatchQueue.main.async to change UI data in main thread.

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/BackgroundThreadBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/BackgroundThreadBootcamp.swift
@@ -14,7 +14,15 @@ class BackgroundThreadViewModel: ObservableObject {
     func fetchData() {
         DispatchQueue.global().async {
             // the self is required! Because it's create a strong referente to the class ViewModel
-            self.dataArray = self.downloadData()
+            let newData = self.downloadData()
+            /**
+             * Problem: Publishing changes from background threads is not allowed; make sure to publish values from the main thread (via operators like receive(on:)) on model updates. In other words, we're not allowed to change dataArray in background thread, because this attribute changes the UI that's use the main thread.
+             *
+             * Solution: Use DispatchQueue.main.async to changes dataArray.
+             */
+            DispatchQueue.main.async {
+                self.dataArray = newData
+            }
         }
     }
     
