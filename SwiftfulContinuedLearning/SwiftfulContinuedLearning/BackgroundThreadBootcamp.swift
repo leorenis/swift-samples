eafc19f6f9b81ab11c28077544c995374a38a173
leorenis <leorenis@gmail.com>
2025-06-12 08:12:15 -0300

    learn: continued learning: Multi-threading with background threads and Queues

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/BackgroundThreadBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundThreadBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 12/06/25.
+//
+
+import SwiftUI
+
+struct BackgroundThreadBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundThreadBootcamp()
+}
