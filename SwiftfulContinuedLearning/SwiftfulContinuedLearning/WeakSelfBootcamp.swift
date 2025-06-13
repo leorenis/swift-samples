c46c3dc9f7868d5eb92ab99e2605c799f1a696bb
leorenis <leorenis@gmail.com>
2025-06-13 09:28:14 -0300

    learn: continued learning: Weak self

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/WeakSelfBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  WeakSelfBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 13/06/25.
+//
+
+import SwiftUI
+
+struct WeakSelfBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeakSelfBootcamp()
+}
