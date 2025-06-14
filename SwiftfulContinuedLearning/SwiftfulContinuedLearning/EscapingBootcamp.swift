760af1e6c4d7642c864eee77ecc87efa52c34ae6
leorenis <leorenis@gmail.com>
2025-06-14 20:40:35 -0300

    learn: continued learning: Escaping. Tips about: Void Functions, Alias parameters

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/EscapingBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/EscapingBootcamp.swift
@@ -3,6 +3,7 @@
 //  SwiftfulContinuedLearning
 //
 //  Created by Leo on 14/06/25.
+//  Episode: https://youtu.be/7gg8iBH2fg4?list=PLwvDm4VfkdpiagxAXCT33Rkwnc5IVhTar
 //
 
 import SwiftUI
@@ -12,13 +13,36 @@ class EscapingViewModel: ObservableObject {
     @Published var text = "Hello"
     
     fileprivate func getData() {
-        let newData = downloadData()
-        text = newData
+        downloadDataAsync { (returnedData) in
+            text = returnedData
+        }
     }
     
     fileprivate func downloadData() -> String {
         return "New data!"
     }
+    
+/// Problem: Does not works, because we cant do this in a synchronous context.
+//    fileprivate func downloadData2() -> String {
+//        // Delay 2 seconds
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//            return "New data!"
+//        }
+//    }
+    
+    /// Solution: Using callback function
+    /// TIPS:
+    ///  - (_ data: String) _ represents a alias for data typed as String outside the function.
+    ///  - In Swift: Closure Functions that returns Void might be represented as: (_ data: String) -> Void, or (_ data: String) -> ()
+    ///  - See more: https://youtu.be/7gg8iBH2fg4?list=PLwvDm4VfkdpiagxAXCT33Rkwnc5IVhTar&t=791
+    fileprivate func downloadDataAsync(completionHandler: (_ data: String) -> Void) {
+        completionHandler("New data!")
+    }
+    
+    /// Sample Void functions
+    func doSomethingVoidFn() {}
+    func doSomethingVoidFnOne(_ data: String) -> () {}
+    func doSomethingVoidFnTwo(_ data: String) -> Void {}
 }
 
 // MARK: STRUCTS
