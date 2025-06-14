fbdea1362d8fcd58ee34ba8128db9e8705abea0f
leorenis <leorenis@gmail.com>
2025-06-14 20:02:07 -0300

    learn: continued learning: Escaping

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/EscapingBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/EscapingBootcamp.swift
@@ -7,12 +7,22 @@
 
 import SwiftUI
 
+// MARK: CLASSES
+class EscapingViewModel: ObservableObject {
+    @Published var text = "Hello"
+}
+
+// MARK: STRUCTS
 struct EscapingBootcamp: View {
+    // MARK: PROPERTIES
+    @StateObject var vm = EscapingViewModel()
+    
     var body: some View {
         Text("Hello, Scaping!")
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     EscapingBootcamp()
 }
