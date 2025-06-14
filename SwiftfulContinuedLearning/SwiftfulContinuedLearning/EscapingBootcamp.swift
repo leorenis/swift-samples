1d404e6ebecb4f3ee8f0fdd9971e79a304bae936
leorenis <leorenis@gmail.com>
2025-06-14 19:53:52 -0300

    learn: continued learning: Escaping

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/EscapingBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  EscapingBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 14/06/25.
+//
+
+import SwiftUI
+
+struct EscapingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EscapingBootcamp()
+}
