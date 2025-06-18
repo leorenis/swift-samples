76e3989b6d271ad565fee49138a34158278ced49
leorenis <leorenis@gmail.com>
2025-06-17 21:28:05 -0300

    learn: continued learning: Codable

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/CodableBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CodableBootcamp.swift
@@ -7,14 +7,25 @@
 
 import SwiftUI
 
-// MARK: CLASSES
-class CodableViewModel: ObservableObject {
-    
+// MARK: MODELS
+struct CustomerModel: Identifiable, Codable {
+    let id: String
+    let name: String
+    let points: Int
+    let isPremium: Bool
 }
 
-// MARK: STRUCTS
+// MARK: CLASSES VIEW MODELS
+@Observable class CodableViewModel {
+    var customer: CustomerModel? = CustomerModel(id: "1", name: "Nick Doe", points: 5, isPremium: true)
+}
+
+// MARK: STRUCTS VIEWS
 /// Structs to learning and training Codable protocol.
 struct CodableBootcamp: View {
+    
+    // MARK: PROPERTIES
+    @State private var vm = CodableViewModel()
     
     // MARK: BODY
     var body: some View {
