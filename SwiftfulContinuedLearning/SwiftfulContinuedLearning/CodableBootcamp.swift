3bbca884c7874a5da961eda65f80842198567d61
leorenis <leorenis@gmail.com>
2025-06-17 21:15:24 -0300

    learn: continued learning: Codable

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CodableBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CodableBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 17/06/25.
+//
+
+import SwiftUI
+
+struct CodableBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodableBootcamp()
+}
