06eaa85b78bdda0c142dec182d6d8cf4eb14a8ad
leorenis <leorenis@gmail.com>
2025-06-19 19:19:38 -0300

    learn: continued learning: Download Data: URLSession.shared.dataTask. Cleaner the code

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadWithEscapingBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadWithEscapingBootcamp.swift
@@ -22,29 +22,33 @@ class DownloadWithEscapingViewModel: ObservableObject {
     fileprivate func getPosts() {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                print("No data.")
+            guard
+                error == nil,
+                let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300
+            else {
+                print("Error downloading data.")
                 return
             }
-            guard error == nil else {
-                print("Error: \(String(describing: error))")
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse else {
-                print( "Invalid HTTPURLResponse.")
-                return
-            }
-            
-            guard response.statusCode >= 200 && response.statusCode < 300 else {
-                print("Status code should be between 200 and 300, but got status code: \(response.statusCode)")
-                return
-            }
-            
-            // print("Successfully downloaded data!")
-            // let jsonString = String(data: data, encoding: .utf8)
-            // print(jsonString as Any)
-            
+//            guard error == nil else {
+//                print("Error: \(String(describing: error))")
+//                return
+//            }
+//            
+//            guard let response = response as? HTTPURLResponse else {
+//                print( "Invalid HTTPURLResponse.")
+//                return
+//            }
+//            
+//            guard response.statusCode >= 200 && response.statusCode < 300 else {
+//                print("Status code should be between 200 and 300, but got status code: \(response.statusCode)")
+//                return
+//            }
+//            print("Successfully downloaded data!")
+//            let jsonString = String(data: data, encoding: .utf8)
+//            print(jsonString as Any)
+
             do {
                 let dataDecoded = try JSONDecoder().decode(PostModel.self, from: data)
                 /// IMPORTANT TIP: Always we manipulate @Published data or any data to our Views (UI), these data must update only from the MAIN Thread, and here we're in a Task in background. So, we MUST to do in main thread using DispatchQueue.main.async and [weak self] as bellow.
