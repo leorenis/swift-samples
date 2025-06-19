7da79eb13f812d266ed575a9223288d9b99bb4f9
leorenis <leorenis@gmail.com>
2025-06-19 11:40:15 -0300

    learn: continued learning: Download Data: URLSession.shared.dataTask. Checks

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadWithEscapingBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadWithEscapingBootcamp.swift
@@ -3,6 +3,7 @@
 //  SwiftfulContinuedLearning
 //
 //  Created by Leo on 19/06/25.
+//  HTTP Response Status Codes: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
 //
 
 import SwiftUI
@@ -16,6 +17,29 @@ class DownloadWithEscapingViewModel: ObservableObject {
     fileprivate func getPosts() {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                print("No data.")
+                return
+            }
+            guard error == nil else {
+                print("Error: \(String(describing: error))")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                print( "Invalid HTTPURLResponse.")
+                return
+            }
+            
+            guard response.statusCode >= 200 && response.statusCode < 300 else {
+                print("Status code should be between 200 and 300, but got status code: \(response.statusCode)")
+                return
+            }
+            
+            print("Successfully downloaded data!")
+            print(data)
+            let jsonString = String(data: data, encoding: .utf8)
+            print(jsonString as Any)
             
         }.resume() // Tip: .resume() here is like the start the function or task here... The task do not starts until call resume.
     }
