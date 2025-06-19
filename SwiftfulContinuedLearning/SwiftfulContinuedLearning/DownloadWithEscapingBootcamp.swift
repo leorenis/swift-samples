7da0b5d628a9368b1a697cc74f4cfc55d151fba9
leorenis <leorenis@gmail.com>
2025-06-19 11:26:22 -0300

    learn: continued learning: Download Data: URLSession.shared.dataTask

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadWithEscapingBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadWithEscapingBootcamp.swift
@@ -7,8 +7,26 @@
 
 import SwiftUI
 
+// MARK: CLASSES
+class DownloadWithEscapingViewModel: ObservableObject {
+    init() {
+        getPosts()
+    }
+    
+    fileprivate func getPosts() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+        }.resume() // Tip: .resume() here is like the start the function or task here... The task do not starts until call resume.
+    }
+}
+
+// MARK: STRUCTS
 /// Struct to learning about download data from the internet using escaping.
 struct DownloadWithEscapingBootcamp: View {
+    // MARK: PROPERTIES
+    @StateObject private var vm = DownloadWithEscapingViewModel()
+    
     // MARK: BODY
     var body: some View {
         Text("Hello, DownloadWithEscaping!")
