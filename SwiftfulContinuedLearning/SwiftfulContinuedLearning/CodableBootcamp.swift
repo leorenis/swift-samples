fe1f955966d17dc197b1331ad675edc9517f37a7
leorenis <leorenis@gmail.com>
2025-06-18 21:34:20 -0300

    learn: continued learning: Codable: Simulates get JSON Data. typealias Encoder

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/CodableBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CodableBootcamp.swift
@@ -39,6 +39,14 @@ struct CustomerModel: Identifiable, Codable {
         self.points = try container.decode(Int.self, forKey: .points)
         self.isPremium = try container.decode(Bool.self, forKey: .isPremium)
     }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(points, forKey: .points)
+        try container.encode(isPremium, forKey: .isPremium)
+    }
 }
 
 // MARK: CLASSES VIEW MODELS
