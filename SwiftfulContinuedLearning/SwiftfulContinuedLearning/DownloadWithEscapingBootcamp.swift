138a24ac226c7fef86593c0cc8ecd84b0c3c3e7c
leorenis <leorenis@gmail.com>
2025-06-19 10:51:31 -0300

    learn: continued learning: Download Data: Escaping!

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadWithEscapingBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadWithEscapingBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 19/06/25.
+//
+
+import SwiftUI
+
+struct DownloadWithEscapingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadWithEscapingBootcamp()
+}
