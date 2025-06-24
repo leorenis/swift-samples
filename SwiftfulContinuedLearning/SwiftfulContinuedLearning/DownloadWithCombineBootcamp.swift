f1ffae107c7c55c6b3db7179f997e0b6f6e9b666
leorenis <leorenis@gmail.com>
2025-06-24 19:52:31 -0300

    learn: continued learning: Download With Combine: setup

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadWithCombineBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadWithCombineBootcamp.swift
@@ -4,15 +4,26 @@
 //
 //  Created by Leo on 24/06/25.
 //
+//  Links:
+//  JSON Placeholder: https://jsonplaceholder.typicode.com
+//  QuickType: https://app.quicktype.io/
+//
 
 import SwiftUI
 
+// MARK: CLASSES
+
+
+// MARK: STRUCTS
+/// Struct to learn about download data with combine
 struct DownloadWithCombineBootcamp: View {
+    // MARK: BODY
     var body: some View {
         Text("Hello, Combine!")
     }
 }
 
+// MARK: PREVIEW
 #Preview {
     DownloadWithCombineBootcamp()
 }
