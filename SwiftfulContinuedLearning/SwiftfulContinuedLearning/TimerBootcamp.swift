451921ecc90bf212874a8ab0ea17cb4cd8060a6d
leorenis <leorenis@gmail.com>
2025-06-25 20:45:43 -0300

    learn: continued learning: Timer using Pub/Sub

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/TimerBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/TimerBootcamp.swift
@@ -2,14 +2,14 @@
 //  TimerBootcamp.swift
 //  SwiftfulContinuedLearning
 //
-//  Created by Leo Renis Santos on 25/06/25.
+//  Created by Leo on 25/06/25.
 //
 
 import SwiftUI
 
 struct TimerBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, Timer!")
     }
 }
 
