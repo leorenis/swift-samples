fe55ef4bbc5a0eb9e31d41b5cbc64d691246bfef
leorenis <leorenis@gmail.com>
2025-06-26 20:52:48 -0300

    learn: continued learning: Timer Using Own Subscriber.

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/SubscriberBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  SubscriberBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 26/06/25.
+//
+
+import SwiftUI
+
+struct SubscriberBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubscriberBootcamp()
+}
