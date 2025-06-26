1dca6e37aa43afd51fe223e460bdd375c58c2576
leorenis <leorenis@gmail.com>
2025-06-25 21:15:53 -0300

    learn: continued learning: Timer using Pub/Sub. OnReceive, DateFormatter

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/TimerBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/TimerBootcamp.swift
@@ -9,6 +9,15 @@ import SwiftUI
 
 /// Struct to training publisher subscriber using onreceive.
 struct TimerBootcamp: View {
+    // MARK: PROPERTIES
+    @State private var currentDate: Date = Date()
+    let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        return formatter
+    }
+    
     // MARK: BODY
     var body: some View {
         ZStack {
@@ -20,12 +29,15 @@ struct TimerBootcamp: View {
             )
             .ignoresSafeArea()
             
-            Text("Hi")
+            Text(dateFormatter.string(from: currentDate))
                 .font(.system(size: 100, weight: .semibold, design: .rounded))
+                .foregroundStyle(.white)
+                .lineLimit(1)
+                .minimumScaleFactor(0.1)
         }
-        
-        
-        
+        .onReceive(timer, perform: { value in
+           currentDate = value
+        })
     }
 }
 
