d92af6f969439822b8d934667f4e77afce8bf509
leorenis <leorenis@gmail.com>
2025-06-27 20:06:37 -0300

    learn: continued learnin. FileManager

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/FileManagerBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  FileManagerBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo on 27/06/25.
+//
+
+import SwiftUI
+
+struct FileManagerBootcamp: View {
+    var body: some View {
+        Text("Hello, Filemanager!")
+    }
+}
+
+#Preview {
+    FileManagerBootcamp()
+}
