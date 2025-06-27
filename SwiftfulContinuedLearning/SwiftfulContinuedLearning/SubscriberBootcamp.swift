69ffb4318e643bae4ee31db111116885d6e8e6ff
leorenis <leorenis@gmail.com>
2025-06-26 21:11:08 -0300

    learn: continued learning: Timer Using Own Subscriber. AnyCancellable.

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/SubscriberBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/SubscriberBootcamp.swift
@@ -6,11 +6,44 @@
 //
 
 import SwiftUI
+import Combine
+
+// MARK: VIEW MODELS
+@Observable class SubscriberViewModel {
+    var count: Int = 0
+    var timer: AnyCancellable?
+    
+    
+    init() {
+        setUpTimer()
+    }
+    
+    func setUpTimer() {
+        timer = Timer
+            .publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+                self.count += 1
+                
+                if self.count >= 10 {
+                    self.timer?.cancel()
+                }
+            }
+    }
+}
 
 /// Struct to learning about Subscriber Pulish using Combine framework.
 struct SubscriberBootcamp: View {
+    // MARK: PROPERTIES
+    @State var vm = SubscriberViewModel()
+    
+    // MARK: BODY
     var body: some View {
-        Text("Hello, Subscriber!")
+        VStack {
+            Text("\(vm.count)")
+                .font(.largeTitle)
+        }
     }
 }
 
