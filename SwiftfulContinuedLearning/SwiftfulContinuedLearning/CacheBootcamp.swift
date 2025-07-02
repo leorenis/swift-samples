d6a0dea116bd7388f217c9add7ab6570028c3f84
leorenis <leorenis@gmail.com>
2025-07-02 20:45:53 -0300

    learn: continued learnin. FileManager Image. Cache

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/CacheBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CacheBootcamp.swift
@@ -2,14 +2,14 @@
 //  CacheBootcamp.swift
 //  SwiftfulContinuedLearning
 //
-//  Created by Leo Renis Santos on 02/07/25.
+//  Created by Leo on 02/07/25.
 //
 
 import SwiftUI
 
 struct CacheBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, Cache!")
     }
 }
 
