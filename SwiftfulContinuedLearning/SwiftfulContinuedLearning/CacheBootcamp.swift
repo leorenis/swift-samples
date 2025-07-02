23f4dad602fc14218a6697004c8d3d98fb477b6a
leorenis <leorenis@gmail.com>
2025-07-02 20:54:43 -0300

    learn: continued learnin. FileManager Image. Cache View

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/CacheBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/CacheBootcamp.swift
@@ -7,12 +7,53 @@
 
 import SwiftUI
 
+// MARK: VIEW
 struct CacheBootcamp: View {
+    // MARK: BODY
     var body: some View {
-        Text("Hello, Cache!")
+        NavigationStack {
+            VStack {
+                Image("dog")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 288, height: 330)
+                    .clipped()
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Text("Save to Cache")
+                            .font(.headline)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundStyle(.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                        
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Delete from Cache")
+                            .font(.headline)
+                            .padding()
+                            .background(Color.red)
+                            .foregroundStyle(.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                        
+                    }
+                }
+
+                Spacer()
+            }
+            .navigationTitle("Cache bootcamp")
+            
+        }
     }
 }
-
+// MARK: PREVIEW
 #Preview {
     CacheBootcamp()
 }
