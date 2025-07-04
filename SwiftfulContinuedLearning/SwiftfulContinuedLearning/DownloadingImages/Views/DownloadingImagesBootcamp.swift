2aa4dbbb80524ecc883e2e7545a2d3bcc81fc7cb
leorenis <leorenis@gmail.com>
2025-07-03 21:31:58 -0300

    learn: continued learning. MiniApp to Fix content

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/Views/DownloadingImagesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadingImagesBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 03/07/25.
+//
+
+import SwiftUI
+
+struct DownloadingImagesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadingImagesBootcamp()
+}
