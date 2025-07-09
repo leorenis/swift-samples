962b8df0e40805737ea56d67792c1dff39615446
leorenis <leorenis@gmail.com>
2025-07-08 22:58:36 -0300

    learn: continued learning. MiniApp - Update ViewModel to use combine and sink data from @Published values in Data Service

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/ViewModels/DownloadingImagesViewModel.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/ViewModels/DownloadingImagesViewModel.swift
@@ -6,8 +6,28 @@
 //
 
 import Foundation
+import Combine
 
 class DownloadingImagesViewModel: ObservableObject {
     @Published var dataArray: [ProductModel] = []
     let dataService = ProductModelDataService.instance
+    var cancellables: Set<AnyCancellable> = []
+    
+    init() {
+        addSubscribers()
+    }
+    
+    /// Subscribes to changes in `productModels` from the `ProductModelDataService` and updates `dataArray`.
+    ///
+    /// This method observes the `productModels` published property using Combine,
+    /// and assigns the latest value to the local `dataArray` whenever it changes.
+    ///
+    /// - Note: The subscription is stored in `cancellables` to maintain its lifecycle.
+    func addSubscribers() {
+        dataService.$productModels
+            .sink { [weak self] (productModels) in
+                self?.dataArray = productModels
+            }
+            .store(in: &cancellables)
+    }
 }
