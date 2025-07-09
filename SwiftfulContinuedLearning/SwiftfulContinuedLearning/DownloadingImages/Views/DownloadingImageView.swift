1d269415f8f8712d664b5f332f4d8e5f37fddc5d
leorenis <leorenis@gmail.com>
2025-07-09 12:06:31 -0300

    learn: continued learning. MiniApp - Image View

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/Views/DownloadingImageView.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadingImageView.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 09/07/25.
+//
+
+import SwiftUI
+
+struct DownloadingImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadingImageView()
+}
