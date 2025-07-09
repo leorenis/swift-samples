2b43e4db5962d266df849397f77a9485c52661a2
leorenis <leorenis@gmail.com>
2025-07-09 11:19:59 -0300

    learn: continued learning. MiniApp - Row View

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/Views/DownloadingImagesRow.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadingImagesRow.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 09/07/25.
+//
+
+import SwiftUI
+
+struct DownloadingImagesRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadingImagesRow()
+}
