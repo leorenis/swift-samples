6d6f22182966975af902ffc5c4e861e8913de485
leorenis <leorenis@gmail.com>
2025-07-08 21:48:25 -0300

    learn: continued learning. MiniApp - View Model definitions

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/ViewModels/DownloadingImagesViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadingImagesViewModel.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 08/07/25.
+//
+
+import SwiftUI
+
+struct DownloadingImagesViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadingImagesViewModel()
+}
