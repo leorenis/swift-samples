4ed0c957b4e33522122bc18a7479d6e7ae7e5165
leorenis <leorenis@gmail.com>
2025-07-09 12:52:26 -0300

    learn: continued learning. MiniApp - Changing view to use ViewModel

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/Views/DownloadingImageView.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/Views/DownloadingImageView.swift
@@ -9,18 +9,30 @@ import SwiftUI
 
 struct DownloadingImageView: View {
     // MARK: PROPERTIES
-    @State var isLoading: Bool = true
+    @StateObject private var loader: ImageLoadingViewModel
+    
+    /// Initializes a new instance of the view with a specified image URL string.
+    ///
+    /// This initializer creates and assigns a `StateObject` that wraps an
+    /// `ImageLoadingViewModel`, which handles loading an image from the given URL.
+    ///
+    /// - Parameter stringURL: The URL string used by the image loading view model to fetch the image.
+    init(stringURL: String) {
+        _loader = StateObject(wrappedValue: ImageLoadingViewModel(url: stringURL))
+    }
     
     var body: some View {
-        if isLoading {
+        if loader.isLoading {
             ProgressView()
-        } else {
-            Circle()
+        } else if let image = loader.image {
+            Image(uiImage: image)
+                .resizable()
+                .clipShape(Circle())
         }
     }
 }
 
 #Preview(traits: .sizeThatFitsLayout, body: {
-    DownloadingImageView()
+    DownloadingImageView(stringURL: "https://cdn.dummyjson.com/product-images/groceries/cat-food/thumbnail.webp")
         .frame(width: 75, height: 75)
 })
