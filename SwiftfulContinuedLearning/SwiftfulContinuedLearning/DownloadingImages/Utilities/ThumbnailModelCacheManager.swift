0e6cfe0a3a47aae3a7caef0a6956d27b0d80df51
leorenis <leorenis@gmail.com>
2025-07-10 07:44:14 -0300

    learn: continued learning. MiniApp - Thumbnail Cache Manager

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/Utilities/ThumbnailModelCacheManager.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/Utilities/ThumbnailModelCacheManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+
+class ThumbnailModelCacheManager {
+    static let instance = ThumbnailModelCacheManager()
+    private init() {}
+    
+    var thumbnailCache: NSCache<NSString, UIImage> = {
+       var cache = NSCache<NSString, UIImage>()
+        cache.countLimit = 100
+        cache.totalCostLimit = 10 * 1024 * 1024 // 10MB
+        return cache
+    }()
+    
+    func add(key: String, value: UIImage) {
+        thumbnailCache.setObject(value, forKey: key as NSString)
+    }
+    
+    func get(key: String) -> UIImage? {
+        return thumbnailCache.object(forKey: key as NSString)
+    }
+}
