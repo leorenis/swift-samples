3f617001ab3b4cbb3ba208fbb85b1decefda6fc9
leorenis <leorenis@gmail.com>
2025-07-11 21:31:58 -0300

    learn: continued learning. Accessibility

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/AccessibilityTextBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityTextBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 11/07/25.
+//
+
+import SwiftUI
+
+struct AccessibilityTextBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessibilityTextBootcamp()
+}
