baaaf2dda1c495bf00db35c8030dabd8b5e8e07a
leorenis <leorenis@gmail.com>
2025-07-12 17:04:58 -0300

    learn: continued learning.Docs

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/ViewModels/DownloadingImagesViewModel.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/DownloadingImages/ViewModels/DownloadingImagesViewModel.swift
@@ -8,11 +8,26 @@
 import Foundation
 import Combine
 
+/// A view model responsible for managing and observing a list of product models with image data.
+///
+/// `DownloadingImagesViewModel` observes changes in the `ProductModelDataService` to maintain an up-to-date list
+/// of `ProductModel` items. It leverages the Combine framework to subscribe to the data stream and automatically
+/// publishes updates to the UI via the `@Published` `dataArray` property.
+///
+/// This view model is suitable for use in SwiftUI views that need to display dynamic lists of products
+/// with images downloaded from the internet.
 class DownloadingImagesViewModel: ObservableObject {
+    /// The array of downloaded `ProductModel` items, published to allow SwiftUI views to update reactively.
     @Published var dataArray: [ProductModel] = []
+    /// A singleton dataService responsible for downloading product data from the internet.
     let dataService = ProductModelDataService.instance
+    
+    /// A set to store Combine subscriptions to keep them alive.
     var cancellables: Set<AnyCancellable> = []
     
+    /// Initializes the view model and sets up subscriptions to data updates.
+    ///
+    /// Upon initialization, the view model begins observing `productModels` from the `dataService`.
     init() {
         addSubscribers()
     }
