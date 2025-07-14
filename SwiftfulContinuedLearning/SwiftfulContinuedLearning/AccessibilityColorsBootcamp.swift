1704f9d4ae2aff6b1ae061f92be201042c4e8dfd
leorenis <leorenis@gmail.com>
2025-07-14 20:54:06 -0300

    learn: continued learninn. Accessibility Colors

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/AccessibilityColorsBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/AccessibilityColorsBootcamp.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct AccessibilityColorsBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button("Button one") {
+                
+            }
+            .foregroundStyle(.primary)
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
