5390ee609ddf74e876b2803cdec0a22014f443d0
leorenis <leorenis@gmail.com>
2025-07-14 21:32:32 -0300

    learn: continued learninn. Accessibility VoiceOver

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/AccessibilityVoiceOverBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityVoiceOverBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 14/07/25.
+//
+
+import SwiftUI
+
+struct AccessibilityVoiceOverBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessibilityVoiceOverBootcamp()
+}
