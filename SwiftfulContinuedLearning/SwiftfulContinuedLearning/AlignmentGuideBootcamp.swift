662c716825e1bfcfd768711b20ba859cc1327915
leorenis <leorenis@gmail.com>
2025-07-20 12:17:02 -0300

    learn: continued learninn. AlignmentGuide. leading

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/AlignmentGuideBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/AlignmentGuideBootcamp.swift
@@ -28,18 +28,34 @@ struct AlignmentGuideBootcamp: View {
 struct AlignmentChildView: View {
     var body: some View {
         VStack (alignment: .leading, spacing: 20) {
-            row(title: "First row")
-            row(title: "Second row")
-            row(title: "Third row")
+            row(title: "First row", showIcon: false)
+            row(title: "Second row", showIcon: true)
+            row(title: "Third row", showIcon: false)
         }
+        .padding(16)
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(radius: 8)
+        .padding(40)
+        
     }
     
-    private func row(title: String) -> some View {
+    private func row(title: String, showIcon: Bool) -> some View {
         HStack(spacing: 10) {
-            Image(systemName: "info.circle")
-                .frame(width: 30, height: 30)
+//            Image(systemName: "info.circle")
+//                .frame(width: 30, height: 30)
+//                .opacity(showIcon ? 1 : 0)
+            if showIcon {
+                Image(systemName: "info.circle")
+                    .frame(width: 30, height: 30)
+            }
             
             Text(title)
+            Spacer()
+        }
+//        .background(.red)
+        .alignmentGuide(.leading) { dimension in
+            return showIcon ? 40 : 0
         }
     }
 }
