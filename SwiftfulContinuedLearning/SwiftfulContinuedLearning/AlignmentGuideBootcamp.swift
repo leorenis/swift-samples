fefd5efa622ab0305ea4ae1767dc86258331dce5
leorenis <leorenis@gmail.com>
2025-07-20 11:49:07 -0300

    learn: continued learninn. AlignmentGuide

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/AlignmentGuideBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AlignmentGuideBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 20/07/25.
+//
+
+import SwiftUI
+
+struct AlignmentGuideBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlignmentGuideBootcamp()
+}
