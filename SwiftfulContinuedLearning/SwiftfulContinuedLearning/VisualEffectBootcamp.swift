d2b1a778b5d8e0f80d4ca322a640608bed1e15f0
leorenis <leorenis@gmail.com>
2025-07-21 21:59:43 -0300

    learn: continued learninn. Visual Effect with geometry

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/VisualEffectBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/VisualEffectBootcamp.swift
@@ -10,6 +10,27 @@ import SwiftUI
 struct VisualEffectBootcamp: View {
     @State private var showSpacer: Bool = false
     var body: some View {
+        visualOne
+        visualScroll
+    }
+    
+    var visualScroll: some View {
+        ScrollView {
+            VStack(spacing: 30) {
+                ForEach(0..<100) { index in
+                    Rectangle()
+                        .frame(width: 300, height: 200)
+                        .frame(maxWidth: .infinity)
+                        .background(.orange)
+                        .visualEffect { content, geometry in
+                            content.offset(x: geometry.frame(in: .global).minY * 0.5)
+                        }
+                }
+            }
+        }
+    }
+    
+    var visualOne: some View {
         VStack {
             Text("Learning Visual Effect!")
                 .padding()
