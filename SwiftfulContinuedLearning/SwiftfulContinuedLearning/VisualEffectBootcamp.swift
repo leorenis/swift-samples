432f46e2b6c4ccd274e7b2db86f54266fab71e05
leorenis <leorenis@gmail.com>
2025-07-21 21:31:38 -0300

    learn: continued learninn. Visual Effect

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/VisualEffectBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/VisualEffectBootcamp.swift
@@ -2,14 +2,14 @@
 //  VisualEffectBootcamp.swift
 //  SwiftfulContinuedLearning
 //
-//  Created by Leo Renis Santos on 21/07/25.
+//  Created by Leo on 21/07/25.
 //
 
 import SwiftUI
 
 struct VisualEffectBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, Visual Effect!")
     }
 }
 
