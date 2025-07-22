6b3c8b69121fd4872f988d8831dc73c973403317
leorenis <leorenis@gmail.com>
2025-07-21 21:31:34 -0300

    learn: continued learninn. Visual Effect

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/VisualEffectBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  VisualEffectBootcamp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Leo Renis Santos on 21/07/25.
+//
+
+import SwiftUI
+
+struct VisualEffectBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VisualEffectBootcamp()
+}
