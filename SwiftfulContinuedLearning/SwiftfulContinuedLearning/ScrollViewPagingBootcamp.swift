69547169cc95ccb929388da7defa20de58569d00
leorenis <leorenis@gmail.com>
2025-07-23 20:57:40 -0300

    learn: continued learning. Scroll view paging

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/ScrollViewPagingBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/ScrollViewPagingBootcamp.swift
@@ -2,14 +2,14 @@
 //  ScrollViewPagingBootcamp.swift
 //  SwiftfulContinuedLearning
 //
-//  Created by Leo Renis Santos on 23/07/25.
+//  Created by Leo on 23/07/25.
 //
 
 import SwiftUI
 
 struct ScrollViewPagingBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, Scroll!")
     }
 }
 
