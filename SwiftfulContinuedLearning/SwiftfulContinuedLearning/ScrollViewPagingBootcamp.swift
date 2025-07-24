8e2d789d89b8f16fa9b13299e3351f72753e5c65
leorenis <leorenis@gmail.com>
2025-07-23 21:17:52 -0300

    learn: continued learning. Scroll view paging. TicTok UI Expirience

--- SwiftfulContinuedLearning/SwiftfulContinuedLearning/ScrollViewPagingBootcamp.swift
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearning/ScrollViewPagingBootcamp.swift
@@ -9,6 +9,28 @@ import SwiftUI
 
 struct ScrollViewPagingBootcamp: View {
     var body: some View {
+        // ScrollViewTicTokUX()
+        ScrollView {
+            ForEach(0..<10) { index in
+                VStack(spacing: 0) {
+                    Rectangle()
+                        //.frame(width: 300, height: 300)
+                        .overlay(Text("\(index)").foregroundStyle(.white))
+                        .frame(maxWidth: .infinity)
+                        //.padding(.vertical, 10)
+                        .containerRelativeFrame(.vertical, alignment: .center)
+                }
+            }
+        }
+        .ignoresSafeArea()
+        .scrollTargetLayout()
+        .scrollTargetBehavior(.paging)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+struct ScrollViewTicTokUX: View {
+    var body: some View {
         ScrollView {
             ForEach(0..<10) { index in
                 VStack(spacing: 0) {
